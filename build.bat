8ea6a125271cd4e8475c3b1a7ba26a83d9cefdcb
DESKTOP-9U44EMC\IssacZhuang <1095313400@qq.com>
2022-12-08 23:21:45 +0800

    first commit

---
+++ build.bat
@@ -0,0 +1,3 @@
+dotnet build Vocore/Vocore.csproj  --configuration Release
+dotnet build Vocore.Compute/Vocore.Compute.csproj  --configuration Release
+dotnet build Vocore.Renderer/Vocore.Renderer.csproj  --configuration Release
