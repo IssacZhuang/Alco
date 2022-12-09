10f7d0c218d86317b5a5bb5c77153cab22c9b68d
庄嘉嵘 <1095313400@qq.com>
2022-12-09 09:55:48 +0800

    opt: auto build

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
-dotnet build Vocore/Vocore.csproj  --configuration Release
-dotnet build Vocore.Compute/Vocore.Compute.csproj  --configuration Release
-dotnet build Vocore.Renderer/Vocore.Renderer.csproj  --configuration Release
+$FolderAssemblies = "./Assemblies/"
+$Modules = @("Vocore", "Vocore.Compute", "Vocore.Renderer")
 
-cp ./Vocore/bin/Release/Vocore.dll ./Assemblies/
-cp ./Vocore.Compute/bin/Release/Vocore.Compute.dll ./Assemblies/
-cp ./Vocore.Renderer/bin/Release/Vocore.Renderer.dll ./Assemblies/
+foreach($module in $Modules){
+    dotnet build $module/$module.csproj  --configuration Release
+    cp ./$module/bin/Release/$module.dll $FolderAssemblies
+}
