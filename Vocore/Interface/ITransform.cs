7d6ec984706350a9a1439c6faf1707643b604dab
IssacZhuang(PC) <1095313400@qq.com>
2022-12-11 22:24:00 +0800

    opt: delete useless method

--- Vocore/Interface/ITransform.cs
+++ Vocore/Interface/ITransform.cs
@@ -10,10 +10,6 @@ namespace Vocore
 {
     public interface ITransform
     {
-        Vector3 Postition { get; set; }
-        Quaternion Rotation { get; set; }
-        Vector3 Scale { get; set; }
-
         Matrix4x4 ToMatrix4x4();
     }
 }
