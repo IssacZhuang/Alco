51f684424964443f45e5b04d662ad326baabb8fe
庄嘉嵘 <1095313400@qq.com>
2022-12-15 11:45:33 +0800

    feature: base framework

---
+++ Vocore/Framework/ISystem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vocore
+{
+    public interface ISystem
+    {
+        void OnCreate();
+        void OnTick();
+        void OnUpdate();
+        void OnDestroy();
+    }
+}
---
+++ Vocore/Framework/ProtoBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vocore
+{
+    public class ProtoBase
+    {
+        public string nameID;
+        public string title;
+        public string desc;
+        
+        public virtual void Initialize()
+        {
+
+        }
+
+        public virtual IEnumerable<string> CheckConfigError()
+        {
+            yield break;
+        }
+    }
+}
