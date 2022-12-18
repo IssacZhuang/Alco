aced6d5dae60799477190e8d4e62b1425603f467
IssacZhuang(PC) <1095313400@qq.com>
2022-12-18 16:17:59 +0800

    feature: parallel job for structured buffer

---
+++ Vocore/Jobs/JobTransMatrixFromBuffer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+using Unity.Jobs;
+
+namespace Vocore
+{
+    public unsafe struct JobTransMatrixFromBuffer : IJobParallelFor
+    {
+        public StructuredBuffer<Vector3> positions;
+        public StructuredBuffer<Quaternion> rotations;
+        public StructuredBuffer<Vector3> scales;
+
+        public StructuredBuffer<Matrix4x4> matrices;
+
+        public void Execute(int index)
+        {
+            matrices[index].SetTRS(positions[index], rotations[index], scales[index]);
+        }
+    }
+}
