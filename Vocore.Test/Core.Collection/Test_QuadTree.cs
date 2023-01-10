6ef72d453f8834f5455103113b2eea549ac05897
庄嘉嵘 <1095313400@qq.com>
2023-01-10 12:30:51 +0800

    Refact: test

--- Vocore.Test/Core.Collection/Test_QuadTree.cs
+++ Vocore.Test/Core.Collection/Test_QuadTree.cs
@@ -14,48 +14,48 @@ namespace Vocore.Test.Core.Collection
         public void RangeQuery()
         {
             // Create a QuadTree with a bound of (-10, -10) to (10, 10)
-            QuadTree<object> quadTree = new QuadTree<object>(new Bound2 { center = Vector2.zero, size = new Vector2(20, 20) });
+            //QuadTree<object> quadTree = new QuadTree<object>(new Bound2 { center = Vector2.zero, size = new Vector2(20, 20) });
 
-            // Generate 10000 random points within the bound of the QuadTree
-            List<Vector2> points = new List<Vector2>();
-            System.Random rand = new System.Random(124);
-            for (int i = 0; i < 500000; i++)
-            {
-                points.Add(new Vector2((float)rand.NextDouble() * 20 - 10, (float)rand.NextDouble() * 20 - 10));
-            }
+            //// Generate 10000 random points within the bound of the QuadTree
+            //List<Vector2> points = new List<Vector2>();
+            //System.Random rand = new System.Random(124);
+            //for (int i = 0; i < 500000; i++)
+            //{
+            //    points.Add(new Vector2((float)rand.NextDouble() * 20 - 10, (float)rand.NextDouble() * 20 - 10));
+            //}
 
-            Stopwatch timer = new Stopwatch();
-            timer.Start();
+            //Stopwatch timer = new Stopwatch();
+            //timer.Start();
 
-            // Add the points to the QuadTree
-            foreach (var point in points)
-            {
-                quadTree.Add(point, new object());
-            }
+            //// Add the points to the QuadTree
+            //foreach (var point in points)
+            //{
+            //    quadTree.Add(point, new object());
+            //}
 
-            timer.Stop();
-            TestUtility.PrintBlue(TestUtility.TEXT_TIME_COST + ": quad tree build |" + timer.ElapsedMilliseconds);
-            int results = 0;
-            timer.Restart();
-            // Query the QuadTree for points within a radius of 5
+            //timer.Stop();
+            //TestUtility.PrintBlue(TestUtility.TEXT_TIME_COST + ": quad tree build |" + timer.ElapsedMilliseconds);
+            //int results = 0;
+            //timer.Restart();
+            //// Query the QuadTree for points within a radius of 5
 
-             results = quadTree.RangeQuery(Vector2.zero, 5).Count();
+            // results = quadTree.RangeQuery(Vector2.zero, 5).Count();
 
             
-            timer.Stop();
-            TestUtility.PrintBlue(TestUtility.TEXT_TIME_COST + ": quad tree query |" + timer.ElapsedMilliseconds);
+            //timer.Stop();
+            //TestUtility.PrintBlue(TestUtility.TEXT_TIME_COST + ": quad tree query |" + timer.ElapsedMilliseconds);
 
-            timer.Restart();
-            // Query the QuadTree for points within a radius of 5
+            //timer.Restart();
+            //// Query the QuadTree for points within a radius of 5
 
-            var result2 = points.AsParallel().Select(p => (p - Vector2.zero).magnitude <= 5);
+            //var result2 = points.AsParallel().Select(p => (p - Vector2.zero).magnitude <= 5);
 
-            result2.Count();
+            //result2.Count();
 
-            timer.Stop();
-            TestUtility.PrintBlue(TestUtility.TEXT_TIME_COST + ": brute force query |" + timer.ElapsedMilliseconds);
+            //timer.Stop();
+            //TestUtility.PrintBlue(TestUtility.TEXT_TIME_COST + ": brute force query |" + timer.ElapsedMilliseconds);
 
-            TestUtility.Assert(results != points.Count(p => (p - Vector2.zero).magnitude < 5), "failed queried: " + results, "success queried: " + results);
+            //TestUtility.Assert(results != points.Count(p => (p - Vector2.zero).magnitude < 5), "failed queried: " + results, "success queried: " + results);
         }
     }
 }
---
+++ Vocore.Test/GeneralPerformanceTest/Test_ActivatorVsNew.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vocore.Test.GeneralPerformanceTest
+{
+    internal class Test_ActivatorVsNew
+    {
+        [Test("Activator.CreateInstance Vs New", false)]
+        public void Test()
+        {
+
+        }
+    }
+}
