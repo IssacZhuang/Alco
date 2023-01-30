34661bbbb997a704d8968673a126fd4c6fd75d92
庄嘉嵘 <1095313400@qq.com>
2023-01-30 22:10:14 +0800

    feature(ADS): smooth follow

---
+++ VodkaRimworld/AirDefenseSystem/CompProperties_TurretAirDefense.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vocore;
+using UnityEngine;
+using Verse;
+
+namespace ADS
+{
+    public class CompProperties_TurretAirDefense:CompProperties
+    {
+        public CompProperties_TurretAirDefense()
+        {
+            compClass = typeof(CompTurretAirDefense);
+        }
+
+        public GraphicData graphicTurretGun;
+        public GraphicData graphicProjectile;
+        public float smoothTime = 0.4f;
+        public float maxAimingSpeed = 18f;
+        public float range = 75;
+
+        private int _projectileRenderID = -1;
+
+        public Material MatTurretGunInt
+        {
+            get
+            {
+                if (graphicTurretGun == null) return null;
+                return graphicTurretGun.Graphic.MatSingle;
+            }
+        }
+
+        public Material MatProjectileInt
+        {
+            get
+            {
+                if (graphicProjectile == null) return null;
+                return graphicProjectile.Graphic.MatSingle;
+            }
+        }
+
+        public int ProjectileRenderID
+        {
+            get {
+                if (_projectileRenderID < 0)
+                {
+                    MatProjectileInt.enableInstancing = true;
+                    _projectileRenderID = InstancedRenderManager.Default.GetRendererID(MeshPool.plane10, MatTurretGunInt);
+                }
+                return _projectileRenderID;
+             }
+        }
+    }
+}
---
+++ VodkaRimworld/AirDefenseSystem/CompTurretAirDefense.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+
+namespace ADS
+{
+    public class CompTurretAirDefense: ThingComp
+    {
+        private float _aimingAngle = 0f;
+        private float _angleVelocity = 0f;
+        private Matrix4x4 _matrix = default;
+        private Thing _target = null;
+
+        public Thing Target
+        {
+            get
+            {
+                return _target;
+            }
+            set
+            {
+                _target = value;
+            }
+        }
+
+        public Vector3 TargetDirection
+        {
+            get
+            {
+                Vector3 direction = UI.MouseMapPosition() - parent.DrawPos;
+                direction.y = 0;
+                return direction;
+            }
+        }
+
+        public float TargetAngle
+        {
+            get
+            {
+                return TargetDirection.AngleFlat();
+            }
+        }
+
+        public CompProperties_TurretAirDefense Props => (CompProperties_TurretAirDefense)props;
+
+        public override void CompTick()
+        {
+            if (Props.graphicTurretGun == null) return;
+            Vector3 drawSize = default;
+            drawSize.Set(Props.graphicTurretGun.drawSize.x, 1, Props.graphicTurretGun.drawSize.y);
+
+            _aimingAngle = Mathf.SmoothDampAngle(_aimingAngle, TargetAngle, ref _angleVelocity, Props.smoothTime, Props.maxAimingSpeed);
+            _matrix.SetTRS(parent.DrawPos, Quaternion.AngleAxis(_aimingAngle,Vector3.up), drawSize);
+        }
+
+        public override void PostDraw()
+        {
+            if (Props.graphicTurretGun == null) return;
+            Graphics.DrawMesh(MeshPool.plane10, _matrix, Props.MatTurretGunInt, 0);
+        }
+
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+            Scribe_Values.Look<float>(ref _aimingAngle, "ADS_aimingAngle");
+        }
+    }
+}
