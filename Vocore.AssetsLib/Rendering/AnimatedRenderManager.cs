081927f51f95030cf8fd36105a4086de54a74fa4
庄嘉嵘 <1095313400@qq.com>
2023-01-31 11:31:02 +0800

    feature(core): abstract render manager

--- Vocore.AssetsLib/Rendering/AnimatedRenderManager.cs
+++ Vocore.AssetsLib/Rendering/AnimatedRenderManager.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 
 namespace Vocore.AssetsLib
 {
-    public class AnimatedRenderManager
+    public class AnimatedRenderManager : BaseInstancedRenderManager<AnimatedRenderQueue>
     {
         private static AnimatedRenderManager _instance;
         public static AnimatedRenderManager Default
@@ -19,64 +19,9 @@ namespace Vocore.AssetsLib
             }
         }
 
-        private readonly List<AnimatedRenderQueue> _renderQueues;
-        private readonly Dictionary<(Mesh, Material), int> _cacheIndex;
-
-        public AnimatedRenderManager()
-        {
-            _renderQueues = new List<AnimatedRenderQueue>();
-            _cacheIndex = new Dictionary<(Mesh, Material), int>();
-        }
-
-        ~AnimatedRenderManager()
-        {
-            for(int i=0; i < _renderQueues.Count; i++)
-            {
-                _renderQueues[i].Dispose();
-            }
-        }
-
-        public int GetRendererID(Mesh mesh, Material material)
-        {
-            if (mesh == null)
-            {
-                throw ExceptionRendering.MeshIsMissing;
-            }
-
-            if(material == null)
-            {
-                throw ExceptionRendering.MaterialIsMissing;
-            }
-
-            if (_cacheIndex.TryGetValue((mesh, material), out int rendererID))
-            {
-                return rendererID;
-            }
-            else
-            {
-                _renderQueues.Add(new AnimatedRenderQueue(mesh, material));
-                rendererID = _renderQueues.Count - 1;
-                _cacheIndex.Add((mesh, material), rendererID);
-                return rendererID;
-            }
-        }
-
         public void AddInstance(int rendererID, Vector3 position, Quaternion rotattion, Vector3 scale, Color color = default, float frame = 0)
         {
             _renderQueues[rendererID].AddInstance(position, rotattion, scale, color, frame);
-        }
-
-        public void Draw()
-        {
-            for(int i = 0; i < _renderQueues.Count; i++)
-            {
-                _renderQueues[i].Draw();
-            }
-        }
-
-        public void Draw(int rendererID)
-        {
-            _renderQueues[rendererID].Draw();
         }
     }
 }
---
+++ Vocore/Rendering/BaseInstancedRenderManager.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Vocore
+{
+    public class BaseInstancedRenderManager<T> where T: InstancedRenderQueue
+    {
+        protected readonly List<T> _renderQueues;
+        protected readonly Dictionary<(Mesh, Material), int> _cacheIndex;
+
+        public BaseInstancedRenderManager()
+        {
+            _renderQueues = new List<T>();
+            _cacheIndex = new Dictionary<(Mesh, Material), int>();
+        }
+
+        ~BaseInstancedRenderManager()
+        {
+            for (int i = 0; i < _renderQueues.Count; i++)
+            {
+                _renderQueues[i].Dispose();
+            }
+        }
+
+        public int GetRendererID(Mesh mesh, Material material)
+        {
+            if (mesh == null)
+            {
+                throw ExceptionRendering.MeshIsMissing;
+            }
+
+            if (material == null)
+            {
+                throw ExceptionRendering.MaterialIsMissing;
+            }
+
+            if (_cacheIndex.TryGetValue((mesh, material), out int rendererID))
+            {
+                return rendererID;
+            }
+            else
+            {
+                T renderQueue = (T)Activator.CreateInstance(typeof(T), new object[] { mesh, material });
+                _renderQueues.Add(renderQueue);
+                rendererID = _renderQueues.Count - 1;
+                _cacheIndex.Add((mesh, material), rendererID);
+                return rendererID;
+            }
+        }
+
+        public void AddInstance(int rendererID, Vector3 position, Quaternion rotattion, Vector3 scale)
+        {
+            _renderQueues[rendererID].AddInstance(position, rotattion, scale);
+        }
+
+        public void Draw()
+        {
+            for (int i = 0; i < _renderQueues.Count; i++)
+            {
+                _renderQueues[i].Draw();
+            }
+        }
+
+        public void Draw(int rendererID)
+        {
+            _renderQueues[rendererID].Draw();
+        }
+    }
+}
