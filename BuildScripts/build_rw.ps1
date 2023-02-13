6b39519784524ac96d5bf649a9f08050d192f590
庄嘉嵘 <1095313400@qq.com>
2023-02-13 11:31:13 +0800

    refact: directory

--- BuildScripts/build_rw.ps1
+++ BuildScripts/build_rw.ps1
@@ -1,10 +1,10 @@
 $FolderAssemblies = "../Assemblies/Rimworld"
-$FolderVodkaTool = "../VodkaRimworld/"
+$FolderRimworld = "../RimAssemblies/"
 $Modules = @("MuzzleFlash", "AirDefenseSystem", "MechTakeAmmoCE")
 
 rm $FolderAssemblies/*.dll
 
 foreach($module in $Modules){
-    dotnet build $FolderVodkaTool/$module/$module.csproj  --configuration Release
-    cp $FolderVodkaTool/$module/bin/Release/$module.dll $FolderAssemblies
+    dotnet build $FolderRimworld/$module/$module.csproj  --configuration Release
+    cp $FolderRimworld/$module/bin/Release/$module.dll $FolderAssemblies
 }
