c6c473a378b340ca03ffa4388e01a094b67f4f3d
庄嘉嵘 <1095313400@qq.com>
2023-03-08 11:42:29 +0800

    opt(muzzel flash): multi-barrel single shot random

--- RimAssemblies/MuzzleFlash/HarmonyPatch/HarmonyPatch_Verb.cs
+++ RimAssemblies/MuzzleFlash/HarmonyPatch/HarmonyPatch_Verb.cs
@@ -50,7 +50,8 @@ namespace MuzzleFlash.Patch
 
             if (muzzleProps.isAlternately)
             {
-                MuzzleFlashUtility.SpawnMuzzleFlash(caster.MapHeld, muzzleProps.def, drawPos, muzzleProps.offsets[___burstShotsLeft % muzzleProps.offsets.Count], direction, muzzleProps.drawSize);
+                int index = __instance.verbProps.burstShotCount == 1 ? Rand.Range(0, muzzleProps.offsets.Count) : ___burstShotsLeft % muzzleProps.offsets.Count;
+                MuzzleFlashUtility.SpawnMuzzleFlash(caster.MapHeld, muzzleProps.def, drawPos, muzzleProps.offsets[index], direction, muzzleProps.drawSize);
             }
             else
             {
