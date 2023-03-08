a764da19845c9d62928cc8a3e6d8f60747628610
庄嘉嵘 <1095313400@qq.com>
2023-03-08 20:35:43 +0800

    array pool

---
+++ Vocore.Test/Core.Collection/Test_Pool.cs
@@ -0,0 +1,40 @@
+namespace Vocore.Test
+{
+    public class Test_Pool
+    {
+        [Test("Test_ArrayPool")]
+        public void Test_ArrayPool()
+        {
+            ArrayPool<object> pool = new ArrayPool<object>(10);
+
+            object[] array = new object[10];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new object();
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                pool.Return(array[i]);
+            }
+
+            pool.Return(new object());
+
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                object item = pool.Get();
+                if (item != array[i])
+                {
+                    TestUtility.AddFailed();
+                    TestUtility.PrintRed("Test_ArrayPool return failed");
+                    return;
+                }
+            }
+
+            TestUtility.Assert(pool.Get() != null, "Test_ArrayPool get failed");
+
+        }
+    }
+}
+
+
