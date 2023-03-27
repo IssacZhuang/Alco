766e2837becfc3418c3a8eea6611f8df37d891eb
庄嘉嵘 <1095313400@qq.com>
2023-03-27 22:49:01 +0800

    refact(curve): interface

--- Vocore.Animation/ICurve.cs
+++ Vocore.Animation/ICurve.cs
@@ -10,5 +10,23 @@ namespace Vocore.Animation
         int PointsCount { get; }
         IList <KeyFrame> Points { get; }
     }
+
+    public interface ICurve2D{
+        Vector2 Evaluate(float t);
+        int PointsCount { get; }
+        IList <KeyFrame2D> Points { get; }
+    }
+
+    public interface ICurve3D{
+        Vector3 Evaluate(float t);
+        int PointsCount { get; }
+        IList <KeyFrame3D> Points { get; }
+    }
+
+    public interface ICurve4D{
+        Vector4 Evaluate(float t);
+        int PointsCount { get; }
+        IList <KeyFrame4D> Points { get; }
+    }
 }
 
