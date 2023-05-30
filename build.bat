6c8124f94591a4a784ec8c204ad68dc59a0ed1eb
zhuang jr <1095313400@qq.com>
2023-05-30 13:41:47 +0800

    move to unity 2022

--- build.bat
+++ build.bat
@@ -9,20 +9,12 @@ if "%1"=="debug" set "mode=Debug"
 
 
 for /f "delims=" %%a in ('dir /b /s Vocore*.csproj ^| findstr /v /i "test"') do (
-
     echo %%a
     dotnet build "%%a" --configuration %mode% --output ./Assemblies
 )
 
-for /f "delims=" %%a in ('dir /b /s RimAssemblies\*.csproj') do (
-    echo %%a
-    dotnet build "%%a" --configuration %mode% --output ./Assemblies/Rimworld
-)
-
 move .\Assemblies.\Vocore.dll .\Assemblies.\0-Vocore.dll
 move .\Assemblies.\Vocore.pdb .\Assemblies.\0-Vocore.pdb
-
-del /q /s .\Assemblies\Rimworld\Vocore*
 
 rmdir /q /s obj
 rmdir /q /s bin
