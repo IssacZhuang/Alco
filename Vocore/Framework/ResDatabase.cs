5940d7a725fc0364371e931178fcf6f1acc9685e
zhuang jr <1095313400@qq.com>
2023-06-27 15:02:33 +0800

    feat: res database get

--- Vocore/Framework/ResDatabase.cs
+++ Vocore/Framework/ResDatabase.cs
@@ -9,18 +9,22 @@ namespace Vocore
     public static class ResDatabase<T> where T : ResBase
     {
         private readonly static List<T> _resList = new List<T>();
-
         public static int Count => _resList.Count;
         public static IEnumerable<T> AllRes => _resList;
 
-        public static void Load(T proto)
+        public static void Load(T res)
         {
-            if (_resList.AsParallel().Any(x => x == proto || x.name == proto.name))
+            if (_resList.AsParallel().Any(x => x == res || x.name == res.name))
             {
-                throw ExceptionFramework.ResAlreadyExist(proto.name);
+                throw ExceptionFramework.ResAlreadyExist(res.name);
             }
 
-            _resList.Add(proto);
+            _resList.Add(res);
+        }
+
+        public static T Get(string name)
+        {
+            return _resList.AsParallel().FirstOrDefault(x => x.name == name);
         }
 
         public static void Clear()
