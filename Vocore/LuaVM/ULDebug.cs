5232fa6754f41eb12260094ffb8557c9cb105119
zhuang jr <1095313400@qq.com>
2023-09-05 12:30:26 +0800

    refact: lua dicrectory

--- Vocore/LuaVM/ULDebug.cs
+++ Vocore/LuaVM/ULDebug.cs
@@ -1,26 +1,18 @@
 ﻿
-#define UNITY
+using System;
+using Logger = Vocore.Log;
 
 namespace UniLua.Tools
 {
-	// using Logger = DebugAssist;
-	using Logger = UnityEngine.Debug;
-
-	// thanks to dharco
-	// refer to https://github.com/dharco/UniLua/commit/2854ddf2500ab2f943f01a6d3c9af767c092ce75
-	public class ULDebug
+	public class LuaLogger
 	{
-		public static System.Action<object> Log = NoAction;
-		public static System.Action<object> LogError = NoAction;
-
-		private static void NoAction(object msg) { }
-
-		static ULDebug()
-		{
-#if UNITY
-			Log = Logger.Log;
-			LogError = Logger.LogError;
-#endif
+        public static void Log(object obj)
+        {
+            Logger.Info(obj);
+        }
+        public static void LogError(object obj)
+        {
+            Logger.Error(obj);
 		}
 	}
 }
