8e92fc381663754937bca1b7a969f8528cfcf363
zhuang jr <1095313400@qq.com>
2023-09-05 16:58:03 +0800

    refact: lua vm

---
+++ Vocore/LuaVM/LuaRuntime.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.IO;
+using System.Reflection;
+using UniLua;
+
+namespace Vocore.Lua
+{
+    public class LuaRuntime
+    {
+        private LuaState luaState;
+		
+        public LuaRuntime()
+        {
+            luaState = new LuaState();
+            luaState.L_OpenLibs();
+        }
+
+        
+    }
+}
+
