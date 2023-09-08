0446a0fbd0c816805987a7733e8a23e63bd8a972
zhuang jr <1095313400@qq.com>
2023-09-08 10:47:33 +0800

    add : lua vm

---
+++ Vocore/LuaVM/LuaRuntime.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.IO;
+using System.Reflection;
+using UniLua;
+
+namespace Vocore.Lua
+{
+    public class LuaRuntime
+    {
+        private readonly LuaState luaState;
+		
+        public LuaRuntime()
+        {
+            luaState = new LuaState();
+            luaState.L_OpenLibs();
+        }
+
+        
+    }
+}
+
---
+++ Vocore/LuaVM/ULDebug.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using Logger = Vocore.Log;
+
+namespace UniLua.Tools
+{
+	public class LuaLogger
+	{
+        public static void Log(object obj)
+        {
+            Logger.Info(obj);
+        }
+        public static void LogError(object obj)
+        {
+            Logger.Error(obj);
+		}
+	}
+}
+
