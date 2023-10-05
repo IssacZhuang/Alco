f52d33a82f0891632eadecba886a62cfc24d5840
zhuang jr <1095313400@qq.com>
2023-10-05 19:54:17 +0800

    remove: unity

--- build.bat
+++ build.bat
@@ -19,18 +19,4 @@ dotnet build Vocore.Framework\Vocore.Framework.csproj --configuration %mode% --o
 rmdir /q /s Vocore\obj
 rmdir /q /s Vocore\bin  
 
-if "%1"=="unity" (
-    mkdir .\UnityContainer\Assets\Plugins
-    del /q /s .\UnityContainer\Assets\Plugins\*
-    copy .\Assemblies\Vocore.dll .\UnityContainer\Assets\Plugins
-    copy .\Assemblies\Vocore.Framework.dll .\UnityContainer\Assets\Plugins
-    copy .\Assemblies\UnityToolBox.dll .\UnityContainer\Assets\Plugins
-)
-
-if "%1"=="game" (
-    copy .\Assemblies\Vocore.dll .\Game\CoreAssemblies\Vocore.dll
-    copy .\Assemblies\Vocore.Framework.dll .\Game\CoreAssemblies\Vocore.Framework.dll
-    copy .\Assemblies\Vocore.Test.Unity.dll .\Game\CoreAssemblies\Vocore.Test.Unity.dll
-) 
-
 
--- test.bat
+++ test.bat
@@ -3,17 +3,6 @@ setlocal
 
 set "mode=Release"
 
-if "%1"=="unity" (
-    mkdir .\Game\CoreAssemblies
-    dotnet build Vocore.Test.Unity/Vocore.Test.Unity.csproj  --configuration %mode% --output ./Assemblies
-    copy .\Assemblies\Vocore.Test.Unity.dll .\Game\CoreAssemblies\Vocore.Test.Unity.dll
-    .\Game\Game.exe test
-    exit
-)
-
-
-if "%1"=="debug" set "mode=Debug"
-
 dotnet build Vocore.Test/Vocore.Test.csproj  --configuration %mode%
 cd Vocore.Test/bin/%mode%/net6.0
 Vocore.Test.exe
