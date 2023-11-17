631b93876bc6b36be5d3b25898941c89b1b1c83b
zhuang jr <1095313400@qq.com>
2023-11-17 10:44:45 +0800

    feature: update example

--- Sandbox/RenderCube/Assets/Basic.hlsl
+++ Sandbox/RenderCube/Assets/Basic.hlsl
@@ -1,25 +1,31 @@
 #include "Core.hlsl"
 
-struct VS_IN {
+struct VS_IN 
+{
   float3 position : POSITION;
   float4 color : COLOR;
   float2 uv : TEXCOORD;
 };
 
-struct PS_IN {
+struct PS_IN 
+{
   float4 position : SV_POSITION;
   float4 color : COLOR;
   float2 uv : TEXCOORD;
 };
 
-PS_IN VS(VS_IN input) {
+PS_IN VS(VS_IN input) 
+{
   PS_IN output = (PS_IN)0;
 
-  output.position = VertexToClipSpace(input.pos);
+  output.position = VertexToClipSpace(input.position);
   output.color = input.color;
   output.uv = input.uv;
 
   return output;
 }
 
-float4 PS(PS_IN input) : SV_Target { return input.color; }
+float4 PS(PS_IN input) : SV_Target 
+{ 
+    return input.color; 
+}
--- Sandbox/RenderCube/Game.cs
+++ Sandbox/RenderCube/Game.cs
@@ -3,13 +3,14 @@ using Vocore.Engine;
 
 public class Game : GameEngine
 {
+    private Shader _shaderBasic;
     public Game(GameEngineSetting setting) : base(setting)
     {
-        
+        _shaderBasic = Shader.Complie(LoadAsset("Shader/Basic.hlsl"), "Basic.hlsl");
     }
     protected override void OnUpdate(float delta)
     {
-        if(Input.IsKeyDown(Key.Escape))
+        if (Input.IsKeyDown(Key.Escape))
         {
             Stop();
         }
@@ -18,5 +19,10 @@ public class Game : GameEngine
         {
             Window.WindowState = Window.WindowState == WindowState.BorderlessFullScreen ? WindowState.Normal : WindowState.BorderlessFullScreen;
         }
+    }
+
+    public string LoadAsset(string path)
+    {
+        return File.ReadAllText(Path.Combine(WorkingDirectory, path));
     }
 }
