fc2ccc426986d303b69121370420054a4823c285
zhuang jr <1095313400@qq.com>
2024-01-12 16:16:11 +0800

    feature: texture view dimension

---
+++ Src/Vocore.Graphics/Enums/BindingType.cs
@@ -0,0 +1,12 @@
+namespace Vocore.Graphics;
+
+public enum BindingType
+{
+    None = 0,
+    UniformBuffer = 1,
+    StorageBuffer = 2,
+    Sampler = 3,
+    SampledImage = 4,
+    Texture = 5,
+    StorageTexture = 6, 
+}
