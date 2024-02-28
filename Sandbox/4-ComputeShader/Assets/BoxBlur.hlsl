c819be0a3179382f0478c870ecfdc1e82dd8d3b3
zhuang jr <1095313400@qq.com>
2024-02-28 12:44:39 +0800

    fix: dxc hlsl compute shader texture load

--- Sandbox/4-ComputeShader/Assets/BoxBlur.hlsl
+++ Sandbox/4-ComputeShader/Assets/BoxBlur.hlsl
@@ -3,7 +3,7 @@
 #define rgba8 [[vk::image_format("rgba8")]]
 
 
-Texture2D<float4> inputTexture : register(t0, space0);
+Texture2D inputTexture : register(t0, space0);
 readonly rgba8 RWTexture2D<float4> outputTexture: register(u0, space1);
 
 cbuffer Constants : register(b0, space2) { int iterations; };
@@ -11,13 +11,12 @@ cbuffer Constants : register(b0, space2) { int iterations; };
 [numthreads(8, 8, 1)] 
 void cs_main(uint3 id: SV_DispatchThreadID) {
   // box blur
-  float4 color = inputTexture[id.xy];
+  float4 color = inputTexture.Load(id.xyz);
 
   int size = iterations;
   for (int i = -size; i <= size; i++) {
     for (int j = -size; j <= size; j++) {
-      int2 pos = int2(id.xy) + int2(i, j);
-      color = color + inputTexture[pos];
+      color = color + inputTexture.Load(id.xyz + int3(i, j, 0));
     }
   }
 
