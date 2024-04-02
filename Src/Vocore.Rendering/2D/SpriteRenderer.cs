ddb3b4781fe91c2d8ad998523efacc4999537cac
zhuang jr <1095313400@qq.com>
2024-04-02 18:07:19 +0800

    feature: sprite renderer

--- Src/Vocore.Rendering/2D/SpriteRenderer.cs
+++ Src/Vocore.Rendering/2D/SpriteRenderer.cs
@@ -1,4 +1,5 @@
 using System.Numerics;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Vocore.Graphics;
 
@@ -11,10 +12,31 @@ public class SpriteRenderer : Renderer
     {
         public Matrix4x4 Model;
         public Vector4 Color;
+        public Rect UvRect;
     }
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct Vertex
+    {
+        public Vector2 Position;
+        public Vector2 TexCoord;
+    }
+
+    private static readonly Vertex[] Vertices =
+   {
+        new Vertex {Position = new Vector2(-0.5f, 0.5f), TexCoord = new Vector2(0, 0)},
+        new Vertex {Position = new Vector2(0.5f, 0.5f), TexCoord = new Vector2(1, 0)},
+        new Vertex {Position = new Vector2(0.5f, -0.5f), TexCoord = new Vector2(1, 1)},
+        new Vertex {Position = new Vector2(-0.5f, -0.5f), TexCoord = new Vector2(0, 1)}
+    };
+
+    private static readonly ushort[] Indices = { 0, 1, 2, 0, 2, 3 };
+    private static readonly Rect DefaultUvRect = new Rect(0, 0, 1, 1);
+
     private readonly GPUCommandBuffer _command;
     private readonly GPUDevice _device;
     private readonly Shader _shader;
+    private readonly Mesh _mesh;
 
     private readonly uint _shaderId_camera;
     private readonly uint _shaderId_texture;
@@ -24,6 +46,7 @@ public class SpriteRenderer : Renderer
         _device = RendereringContext.Device;
         _command = _device.CreateCommandBuffer();
         _shader = shader;
+        _mesh = Mesh.Create(Vertices, Indices);
 
         _shaderId_camera = _shader.GetResourceId("Camera");
         _shaderId_texture = _shader.GetResourceId("Texture");
@@ -33,6 +56,10 @@ public class SpriteRenderer : Renderer
     {
         _command.Begin();
         _command.SetFrameBuffer(target);
+        _command.SetGraphicsPipeline(_shader.Pipeline);
+        _command.SetGraphicsResources(_shaderId_camera, Camera.ViewProjectionBuffer);
+        _command.SetVertexBuffer(0, _mesh.VertexBuffer);
+        _command.SetIndexBuffer(_mesh.IndexBuffer, _mesh.IndexFormat);
     }
 
     public void End()
@@ -41,20 +68,102 @@ public class SpriteRenderer : Renderer
         _device.Submit(_command);
     }
 
+    #region  Draw 3D
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Draw(Texture2D texture, Vector3 position, Quaternion rotation, Vector3 scale, Vector4 color)
+    {
+        Transform3D transform = new Transform3D(position, rotation, scale);
+        DrawCore(texture, DefaultUvRect, transform.Matrix, color);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Draw(Texture2D texture, Vector3 position, Quaternion rotation, Vector3 scale, Rect uvRect, Vector4 color)
+    {
+        Transform3D transform = new Transform3D(position, rotation, scale);
+        DrawCore(texture, uvRect, transform.Matrix, color);
+    }
+
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Draw(Texture2D texture, Transform3D transform, Vector4 color)
+    {
+        DrawCore(texture, DefaultUvRect, transform.Matrix, color);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Draw(Texture2D texture, Transform3D transform, Rect uvRect, Vector4 color)
+    {
+        DrawCore(texture, uvRect, transform.Matrix, color);
+    }
+
+    #endregion
+
+    #region Draw 2D
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Draw(Texture2D texture, Vector2 position, Rotation2D rotation, Vector2 scale, Vector4 color)
+    {
+        Transform2D transform = new Transform2D(position, rotation, scale);
+        DrawCore(texture, DefaultUvRect, transform.Matrix, color);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Draw(Texture2D texture, Vector2 position, Rotation2D rotation, Vector2 scale, Rect uvRect, Vector4 color)
+    {
+        Transform2D transform = new Transform2D(position, rotation, scale);
+        DrawCore(texture, uvRect, transform.Matrix, color);
+    }
+
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Draw(Texture2D texture, Transform2D transform, Vector4 color)
+    {
+        DrawCore(texture, DefaultUvRect, transform.Matrix, color);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Draw(Texture2D texture, Transform2D transform, Rect uvRect, Vector4 color)
+    {
+        DrawCore(texture, uvRect, transform.Matrix, color);
+    }
+
+    #endregion
+
+    
+    #region Draw by matrix
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public void Draw(Texture2D texture, Matrix4x4 matrix, Vector4 color)
+    {
+        DrawCore(texture, DefaultUvRect, matrix, color);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Draw(Texture2D texture, Matrix4x4 matrix, Rect uvRect, Vector4 color)
+    {
+        DrawCore(texture, uvRect, matrix, color);
+    }
+
+    #endregion
+
+    private void DrawCore(Texture2D texture, Rect uvRect, Matrix4x4 matrix, Vector4 color)
     {
         Constant constant = new Constant
         {
             Model = matrix,
-            Color = color
+            Color = color,
+            UvRect = uvRect
         };
 
-        _command.SetGraphicsPipeline(_shader.Pipeline);
-
+        _command.SetGraphicsResources(_shaderId_texture, texture.EntrySample);
+        _command.PushConstants(ShaderStage.Vertex, constant);
+        _command.DrawIndexed(_mesh.IndexCount, 1, 0, 0, 0);
     }
 
     protected override void Dispose(bool disposing)
     {
-        
+        _command.Dispose();
+        _mesh.Dispose();
     }
 }
--- Src/Vocore.Rendering/Data/Vertex3D.cs
+++ Src/Vocore.Rendering/Data/Vertex3D.cs
@@ -4,15 +4,15 @@ using Vocore.Unsafe;
 
 namespace Vocore.Rendering;
 
-public struct Vertex
+public unsafe struct Vertex3D
 {
-    public static readonly uint SizeInBytes = (uint)UtilsMemory.SizeOf<Vertex>();
+    public static readonly int SizeInBytes = sizeof(Vertex3D);
 
     public Vector3 position;
     public Vector2 uv;
     public Vector4 color;
 
-    public Vertex(Vector3 position, Vector2 uv, Vector4 color)
+    public Vertex3D(Vector3 position, Vector2 uv, Vector4 color)
     {
         this.position = position;
         this.uv = uv;
