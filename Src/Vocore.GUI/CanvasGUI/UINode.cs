1e4c6270a06fa3437d24f56ee7c4b6dafd857f11
zhuang jr <1095313400@qq.com>
2024-05-08 15:36:08 +0800

    wip: UINode

---
+++ Src/Vocore.GUI/CanvasGUI/UINode.cs
@@ -0,0 +1,231 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace Vocore.GUI;
+
+public abstract class UINode
+{
+    private readonly List<UINode> _children = new();
+    private Matrix4x4 _transformCache = Matrix4x4.Identity;
+    private Transform2D _transform = Transform2D.Identity;
+    private bool _isDirty = true;
+    public Pivot pivot = Pivot.Center;
+    public Anchor anchor = Anchor.Center;
+
+    public UINode? Parent { get; set; } = null;
+    public string Name { get; set; } = string.Empty;
+    public bool IsVisible { get; set; } = true;
+
+    public Vector2 Position
+    {
+        get => _transform.position;
+        set
+        {
+            _transform.position = value;
+            _isDirty = true;
+        }
+    }
+
+    public Rotation2D rotation
+    {
+        get => _transform.rotation;
+        set
+        {
+            _transform.rotation = value;
+            _isDirty = true;
+        }
+    }
+
+    public Vector2 Size
+    {
+        get => _transform.scale;
+        set
+        {
+            _transform.scale = value;
+            _isDirty = true;
+        }
+    }
+    
+    public UINode this[string name]
+    {
+        get
+        {
+            UINode? node = _children.FirstOrDefault(x => x.Name == name) ?? throw new KeyNotFoundException($"The name {name} does not exist in the list.");
+            return node;
+        }
+    }
+
+    #region Add/Remove
+
+    public void Add(UINode node)
+    {
+        if (node.Parent != null)
+        {
+            throw new ArgumentException($"The node {node.Name} already has a parent.");
+        }
+
+        if (node.Parent == this)
+        {
+            throw new ArgumentException($"The node {node.Name} is already a child of this node.");
+        }
+
+        node.Parent = this;
+        _children.Add(node);
+    }
+
+    public bool TryAdd(UINode node)
+    {
+        if (node.Parent != null)
+        {
+            return false;
+        }
+
+        if (node.Parent == this)
+        {
+            return false;
+        }
+
+        node.Parent = this;
+        _children.Add(node);
+        return true;
+    }
+
+    public void Remove(UINode node)
+    {
+        if (node.Parent != this)
+        {
+            throw new ArgumentException($"The node {node.Name} is not a child of this node.");
+        }
+
+        node.Parent = null;
+        _children.Remove(node);
+    }
+
+    public bool TryRemove(UINode node)
+    {
+        if (node.Parent != this)
+        {
+            return false;
+        }
+
+        node.Parent = null;
+        _children.Remove(node);
+        return true;
+    }
+
+    #endregion
+
+    #region Get
+
+    public UINode Get(string name)
+    {
+        UINode? node = _children.FirstOrDefault(x => x.Name == name) ?? throw new KeyNotFoundException($"The name {name} does not exist in the list.");
+        return node;
+    }
+
+    public UINode Get(int index)
+    {
+        return _children[index];//throw inside the list
+    }
+
+    public bool TryGet(string name, [MaybeNullWhen(false)] out UINode? node)
+    {
+        node = _children.FirstOrDefault(x => x.Name == name);
+        return node != null;
+    }
+
+    public bool TryGet(int index, [MaybeNullWhen(false)] out UINode? node)
+    {
+        if (index < 0 || index >= _children.Count)
+        {
+            node = null;
+            return false;
+        }
+        node = _children[index];
+        return true;
+    }
+
+    //generic
+
+    public T Get<T>(string name) where T : UINode
+    {
+        UINode node = Get(name);
+        if (node is T t)
+        {
+            return t;
+        }
+        throw new InvalidCastException($"The node {name} is not of type {typeof(T).Name}.");
+    }
+
+    public T Get<T>(int index) where T : UINode
+    {
+        UINode node = Get(index);
+        if (node is T t)
+        {
+            return t;
+        }
+        throw new InvalidCastException($"The node at index {index} is not of type {typeof(T).Name}.");
+    }
+
+    public bool TryGet<T>(string name, [MaybeNullWhen(false)] out T? node) where T : UINode
+    {
+        if (TryGet(name, out UINode? n) && n is T t)
+        {
+            node = t;
+            return true;
+        }
+        node = null;
+        return false;
+    }
+
+    public bool TryGet<T>(int index, [MaybeNullWhen(false)] out T? node) where T : UINode
+    {
+        if (TryGet(index, out UINode? n) && n is T t)
+        {
+            node = t;
+            return true;
+        }
+        node = null;
+        return false;
+    }
+
+    #endregion
+
+    public abstract void OnTick(float delta);
+    public abstract void OnUpdate(float delta);
+
+    internal void InternalTick(float delta)
+    {
+        OnTick(delta);
+        for (int i = 0; i < _children.Count; i++)
+        {
+            _children[i].InternalTick(delta);
+        }
+    }
+
+    
+
+    internal void InternalUpdate(float delta)
+    {
+        OnUpdate(delta);
+
+        if (!IsVisible)
+        {
+            return;
+        }
+        for (int i = 0; i < _children.Count; i++)
+        {
+            _children[i].InternalUpdate(delta);
+        }
+    }
+
+    internal void InternalResize(Vector2 size)
+    {
+        
+        for (int i = 0; i < _children.Count; i++)
+        {
+            _children[i].InternalResize(size);
+        }
+    }
+}
