7485f635ecef428364283baa7338c9af66ed8183
zhuang jr <1095313400@qq.com>
2024-05-14 18:32:55 +0800

    feature: UtillsEasing

--- Src/Vocore.GUI/CanvasGUI/UIBUtton.cs
+++ Src/Vocore.GUI/CanvasGUI/UIBUtton.cs
@@ -92,6 +92,7 @@ public class UIButton : UISelectable
 
         _tTransition += delta / FadeDuration;
         float t = math.clamp(_tTransition, 0, 1);
+        t = UtilsEasing.QuintInOut(t);
         if ((_transitionMode & TransitionMode.ColorTint) != 0)
         {
             UpdateColorTween(t);
