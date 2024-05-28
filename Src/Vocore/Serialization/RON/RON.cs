afcfbc7791151eab276f5d2cdef106df72d07df2
zhuang jr <1095313400@qq.com>
2024-05-28 18:32:23 +0800

    wip: ron test

--- Src/Vocore/Serialization/RON/RON.cs
+++ Src/Vocore/Serialization/RON/RON.cs
@@ -7,13 +7,7 @@ namespace Ron.NET;
 
 public static class RON
 {
-    private static RonGenMethods _genMethods;
 
-    public static void Init(RonGenMethods methods)
-    {
-        _genMethods = methods;
-    }
-    
     public static IElement Parse(string ron)
     {
         RonParser parser = new RonParser(ron);
@@ -23,15 +17,6 @@ public static class RON
         return ParseElement(tokens);
     }
 
-    public static T Deserialize<T>(string ron)
-    {
-        return (T) _genMethods.Methods[typeof(T)].deserialize(ron);
-    }
-
-    public static string Serialize(object obj, SerializeOptions options = SerializeOptions.None)
-    {
-        return IElement.Serialize(_genMethods.Methods[obj.GetType()].serialize(obj), options);
-    }
 
     private static IElement ParseElement(Token[] tokens)
     {
