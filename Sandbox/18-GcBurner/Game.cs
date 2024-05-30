56880e48f299b381555093fb16084e25d5b57728
zhuang jr <1095313400@qq.com>
2024-05-30 20:12:22 +0800

    opt: gc burner sandbox

--- Sandbox/18-GcBurner/Game.cs
+++ Sandbox/18-GcBurner/Game.cs
@@ -5,35 +5,23 @@ using Vocore;
 using Vocore.Rendering;
 using Vocore.GUI;
 
+
+/*Note: 
+
+current problem:
+collocter large amount of GPU will block the GPUQueue
+
+*/
+
 public class Game : GameEngine
 {
-    public const int Iteration = 1;
-    public const float TickInterval = 0.5f;
-
-    private float _timer = 0;
 
     public Game(GameEngineSetting setting) : base(setting)
     {
 
     }
 
-    protected override void OnTick(float delta)
-    {
-        _timer += delta;
-        if (_timer > TickInterval)
-        {
-            _timer -= TickInterval;
-            for (int i = 0; i < Iteration; i++)
-            {
-                //test the garbage collector for both RAM and VRAM
-                //load asset without cache
-                Assets.Load<Font>("Font/Default.ttf", AssetCacheMode.None);
-                Rendering.CreateGraphicsArrayBuffer<Vector3>(1000);
-                Rendering.CreateRenderTexture(Rendering.DefaultRenderPass, 1280, 720);
-            }
-        }
 
-    }
 
     override protected void OnUpdate(float delta)
     {
@@ -42,7 +30,34 @@ public class Game : GameEngine
             Stop();
         }
 
+
+
         DebugGUI.Text(FrameRate);
+
+        if (DebugGUI.Button("Alloc 1"))
+        {
+            for (int i = 0; i < 1; i++)
+            {
+                AllocResource();
+            }
+        }
+
+        if (DebugGUI.Button("Alloc 10"))
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                AllocResource();
+            }
+        }
+
+        if (DebugGUI.Button("Alloc 100"))
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                AllocResource();
+            }
+        }
+
         if (DebugGUI.Button("Collect Gen 0"))
         {
             GC.Collect(0);
@@ -58,14 +73,23 @@ public class Game : GameEngine
             GC.Collect(2);
         }
 
-        if (DebugGUI.Button("Collect All"))
-        {
-            GC.Collect();
-        }
+        // if (DebugGUI.Button("Collect All"))
+        // {
+        //     GC.Collect();
+        // }
     }
 
     protected override void OnStop()
     {
 
+    }
+
+    private void AllocResource()
+    {
+        //test the garbage collector for both RAM and VRAM
+        //load asset without cache
+        Assets.Load<Font>("Font/Default.ttf", AssetCacheMode.None);
+        Rendering.CreateGraphicsArrayBuffer<Vector3>(1000);
+        Rendering.CreateRenderTexture(Rendering.DefaultRenderPass, 1280, 720);
     }
 }
