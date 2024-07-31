ebf20cd5400e199828c02ae413c743cdcfaca7a1
zhuang jr <1095313400@qq.com>
2024-07-31 23:25:56 +0800

    wip: slang managed compiler

--- Src/Vocore.ShaderCompiler/Binding/Slang/Managed/SlangCompileOption.cs
+++ Src/Vocore.ShaderCompiler/Binding/Slang/Managed/SlangCompileOption.cs
@@ -4,6 +4,13 @@ namespace SlangSharp;
 
 public struct SlangCompileOption
 {
+    public static readonly SlangCompileOption Default = new SlangCompileOption{
+        OptimizationLevel = SlangOptimizationLevel.SLANG_OPTIMIZATION_LEVEL_MAXIMAL,
+        CompileTarget = SlangCompileTarget.SLANG_SPIRV,
+        TargetFlags = SlangTargetFlags.SLANG_TARGET_FLAG_GENERATE_SPIRV_DIRECTLY,
+        SourceLanguage = SlangSourceLanguage.SLANG_SOURCE_LANGUAGE_SLANG,
+    };
+
     public SlangOptimizationLevel? OptimizationLevel;
     public SlangCompileTarget? CompileTarget;
     public SlangTargetFlags? TargetFlags;
--- Src/Vocore.ShaderCompiler/Binding/Slang/Managed/SlangCompiler.cs
+++ Src/Vocore.ShaderCompiler/Binding/Slang/Managed/SlangCompiler.cs
@@ -1,16 +1,26 @@
+using Vocore.Graphics;
+using Vocore.ShaderCompiler;
 using static SlangSharp.Slang;
 
 namespace SlangSharp;
 
+public struct SlangCompileResult
+{
+    public string EntryPoint;
+    public ShaderStage Stage;
+    public byte[] Spirv;
+}
+
 public class SlangCompiler : IDisposable
 {
     private readonly SlangSession _session;
-    private readonly BaseSlangFileSystem? _fileSystem;
+    public BaseSlangFileSystem? FileSystem { get; set; }
+
 
     public SlangCompiler(BaseSlangFileSystem fileSystem, string sessionName = "unnamed_session")
     {
         _session = spCreateSession(sessionName);
-        _fileSystem = fileSystem;
+        FileSystem = fileSystem;
     }
 
     public SlangCompiler(string sessionName = "unnamed_session")
@@ -18,10 +28,140 @@ public class SlangCompiler : IDisposable
         _session = spCreateSession(sessionName);
     }
 
+    public SlangCompileResult[] Compile(string path, string code, SlangCompileOption? option = null)
+    {
+        SlangCompileOption compileOption = option ?? SlangCompileOption.Default;
+        SlangSourceLanguage sourceLanguage = compileOption.SourceLanguage ?? SlangSourceLanguage.SLANG_SOURCE_LANGUAGE_SLANG;
 
+        SlangCompileRequest request = MakeRequest(compileOption);
+
+        int translationUnitIndex = spAddTranslationUnit(request, sourceLanguage, path);
+        spAddTranslationUnitSourceString(request, translationUnitIndex, path, code);
+
+        SlangResult result = spCompile(request);
+
+        if (result.IsError)
+        {
+            throw new ShaderCompilationException(request.GetDiagnosticString());
+        }
+
+        SlangReflection reflection = spGetReflection(request);
+
+        var entryCount = spReflection_getEntryPointCount(reflection);
+
+        SlangCompileResult[] results = new SlangCompileResult[entryCount];
+
+
+        for (uint i = 0; i < entryCount; i++)
+        {
+            SlangReflectionEntryPoint entryPoint = spReflection_getEntryPointByIndex(reflection, i);
+            SlangStage entryStage = spReflectionEntryPoint_getStage(entryPoint);
+
+            results[i] = new SlangCompileResult
+            {
+                Spirv = request.GetBytesByEntryPointIndex((int)i),
+                Stage = ConvertShaderStage(entryStage),
+                EntryPoint = entryPoint.GetName()
+            };
+        }
+
+        spDestroyCompileRequest(request);
+
+        return results;
+    }
 
     public void Dispose()
     {
+        spDestroySession(_session);
+    }
 
+    private SlangCompileRequest MakeRequest(SlangCompileOption compileOption)
+    {
+        SlangCompileRequest request = spCreateCompileRequest(_session);
+
+
+        int targetIndex;
+
+        if (compileOption.CompileTarget.HasValue)
+        {
+            targetIndex = spAddCodeGenTarget(request, compileOption.CompileTarget.Value);
+        }
+        else
+        {
+            targetIndex = spAddCodeGenTarget(request, SlangCompileTarget.SLANG_SPIRV);
+        }
+
+        if (compileOption.TargetFlags.HasValue)
+        {
+            spSetTargetFlags(request, targetIndex, compileOption.TargetFlags.Value);
+        }
+
+        if (compileOption.OptimizationLevel.HasValue)
+        {
+            spSetOptimizationLevel(request, compileOption.OptimizationLevel.Value);
+        }
+
+        if (compileOption.LineDirectiveMode.HasValue)
+        {
+            spSetLineDirectiveMode(request, compileOption.LineDirectiveMode.Value);
+        }
+
+        if (compileOption.MatrixLayoutMode.HasValue)
+        {
+            spSetMatrixLayoutMode(request, compileOption.MatrixLayoutMode.Value);
+        }
+
+        if (compileOption.PassThrough.HasValue)
+        {
+            spSetPassThrough(request, compileOption.PassThrough.Value);
+        }
+
+        if (compileOption.FloatingPointMode.HasValue)
+        {
+            spSetTargetFloatingPointMode(request, targetIndex, compileOption.FloatingPointMode.Value);
+        }
+
+        if (compileOption.DebugInfoLevel.HasValue)
+        {
+            spSetDebugInfoLevel(request, compileOption.DebugInfoLevel.Value);
+        }
+
+        if (compileOption.DebugInfoFormat.HasValue)
+        {
+            spSetDebugInfoFormat(request, compileOption.DebugInfoFormat.Value);
+        }
+
+        if (compileOption.DiagnosticFlags.HasValue)
+        {
+            spSetDiagnosticFlags(request, compileOption.DiagnosticFlags.Value);
+        }
+
+        return request;
+    }
+
+    private static ShaderStage ConvertShaderStage(SlangStage stage)
+    {
+        return stage switch
+        {
+            SlangStage.SLANG_STAGE_VERTEX => ShaderStage.Vertex,
+            SlangStage.SLANG_STAGE_FRAGMENT => ShaderStage.Fragment,
+            SlangStage.SLANG_STAGE_COMPUTE => ShaderStage.Compute,
+            SlangStage.SLANG_STAGE_HULL => ShaderStage.Hull,
+            SlangStage.SLANG_STAGE_DOMAIN => ShaderStage.Domain,
+            _ => throw new NotSupportedException("Unsupported shader stage")
+        };
+    }
+
+    private static SlangStage ConvertShaderStage(ShaderStage stage)
+    {
+        return stage switch
+        {
+            ShaderStage.Vertex => SlangStage.SLANG_STAGE_VERTEX,
+            ShaderStage.Fragment => SlangStage.SLANG_STAGE_FRAGMENT,
+            ShaderStage.Compute => SlangStage.SLANG_STAGE_COMPUTE,
+            ShaderStage.Hull => SlangStage.SLANG_STAGE_HULL,
+            ShaderStage.Domain => SlangStage.SLANG_STAGE_DOMAIN,
+            _ => throw new NotSupportedException("Unsupported shader stage")
+        };
     }
 }
