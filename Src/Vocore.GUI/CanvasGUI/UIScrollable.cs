b02a7a085d4db54f703593a28343d66afb880540
zhuang jr <1095313400@qq.com>
2024-08-04 22:57:07 +0800

    feature: ui scroll position calc

--- Src/Vocore.GUI/CanvasGUI/UIScrollable.cs
+++ Src/Vocore.GUI/CanvasGUI/UIScrollable.cs
@@ -74,42 +74,73 @@ public class UIScrollable : UISelectable
 
     private void SetContentPosition(Vector2 position)
     {
-        BoundingBox2D bound = Bound;
-        Vector2 contentSize = _content!.Size;
-        Vector2 contentOffset = _content!.Size * _content.Pivot;
-        Vector2 contentPosition = _content.Position;
-        bound.min -= contentOffset;
-        bound.max -= contentOffset;
+        BoundingBox2D boundSelf = GetLocalBound(this);
+        BoundingBox2D boundContent = GetLocalBound(_content!);
+        BoundingBox2D boundPosition = new BoundingBox2D()
+        {
+            min = boundSelf.min - boundContent.min,
+            max = boundSelf.max - boundContent.max,
+        };
 
+        if (boundPosition.min.X > 0)
+        {
+            boundPosition.min.X = 0;
+        }
+
+        if (boundPosition.min.Y > 0)
+        {
+            boundPosition.min.Y = 0;
+        }
+
+        if (boundPosition.max.X < 0)
+        {
+            boundPosition.max.X = 0;
+        }
+
+        if (boundPosition.max.Y < 0)
+        {
+            boundPosition.max.Y = 0;
+        }
+
+        DebugGUI.Text(boundSelf.ToString());
+        DebugGUI.Text(boundContent.ToString());
+        DebugGUI.Text(boundPosition.ToString());
 
         if ((ScrollMode & SrollMode.Vertical) != 0)
         {
-            _content.Position = new Vector2(_content.Position.X, position.Y);
+            _content!.Position = new Vector2(_content.Position.X, position.Y);
 
             //clmap
-            // if (_content.Position.Y < bound.min.Y)
-            // {
-            //     _content.Position = new Vector2(_content.Position.X, bound.min.Y);
-            // }
-            // else if (_content.Position.Y > bound.max.Y)
-            // {
-            //     _content.Position = new Vector2(_content.Position.X, bound.max.Y);
-            // }
+            if (_content.Position.Y < boundPosition.min.Y)
+            {
+                _content.Position = new Vector2(_content.Position.X, boundPosition.min.Y);
+            }
+            else if (_content.Position.Y > boundPosition.max.Y)
+            {
+                _content.Position = new Vector2(_content.Position.X, boundPosition.max.Y);
+            }
         }
 
         if ((ScrollMode & SrollMode.Horizontal) != 0)
         {
-            _content.Position = new Vector2(position.X, _content.Position.Y);
+            _content!.Position = new Vector2(position.X, _content.Position.Y);
 
             //clmap
-            // if (_content.Position.X < bound.min.X)
-            // {
-            //     _content.Position = new Vector2(bound.min.X, _content.Position.Y);
-            // }
-            // else if (_content.Position.X > bound.max.X)
-            // {
-            //     _content.Position = new Vector2(bound.max.X, _content.Position.Y);
-            // }
+            if (_content.Position.X < boundPosition.min.X)
+            {
+                _content.Position = new Vector2(boundPosition.min.X, _content.Position.Y);
+            }
+            else if (_content.Position.X > boundPosition.max.X)
+            {
+                _content.Position = new Vector2(boundPosition.max.X, _content.Position.Y);
+            }
         }
+    }
+
+    private static BoundingBox2D GetLocalBound(UINode node)
+    {
+        Transform2D transform = node.RenderTransform;
+        Vector2 halfSize = transform.scale * 0.5f;
+        return new BoundingBox2D(halfSize, -halfSize);
     }
 }
