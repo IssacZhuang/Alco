f8f2fee127e1dfa41eacaa9337c63da53ae5f99a
zhuang jr <1095313400@qq.com>
2024-08-15 18:46:26 +0800

    update comment

--- Src/Vocore.IO/BaseAssetLoader.cs
+++ Src/Vocore.IO/BaseAssetLoader.cs
@@ -12,10 +12,10 @@ public abstract class BaseAssetLoader<TAsset> : IAssetLoader<TAsset> where TAsse
 
 
     /// <summary>
-    /// Tries to create an asset from the specified filename and preprocessed data. This method will execute on main thread when preprocessed data is ready.
+    /// Tries to create an asset from the file. It might be executed on a background thread so it has to be thread-safe.
     /// </summary>
     /// <param name="filename">The filename of the asset.</param>
-    /// <param name="preprocessed">The preprocessed data.</param>
+    /// <param name="file">The file data.</param>
     /// <param name="asset">When this method returns, contains the created asset if the asset creation was successful; otherwise, <c>null</c>.</param>
     /// <returns><c>true</c> if the asset was successfully created; otherwise, <c>false</c>.</returns>
     public bool TryCreateAsset(string filename, ReadOnlySpan<byte> file, [NotNullWhen(true)] out TAsset? asset)
