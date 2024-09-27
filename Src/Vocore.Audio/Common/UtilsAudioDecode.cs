6958fb3991c1b79665ed698ea193d2c4ecc0abf5
zhuang jr <1095313400@qq.com>
2024-09-27 17:08:39 +0800

    feature: ogg interleaved decode simd

--- Src/Vocore.Audio/Common/UtilsAudioDecode.cs
+++ Src/Vocore.Audio/Common/UtilsAudioDecode.cs
@@ -1,4 +1,5 @@
 using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics;
 using Hebron.Runtime;
 using StbVorbisSharp;
 
@@ -6,6 +7,7 @@ namespace Vocore.Audio;
 
 public unsafe static class UtilsAudioDecode
 {
+    public const float Int16ToFloat32 = 1 / 32768f;
     public static AudioClip DecodeOgg(ReadOnlySpan<byte> data, bool interleaved = true)
     {
         short* result = null;// the result of stb vorbis is interleaved
@@ -30,21 +32,54 @@ public unsafe static class UtilsAudioDecode
         if (interleaved)
         {
             //use original data
-            for (int i = 0; i < length; i++)
+            // for (int i = 0; i < length; i++)
+            // {
+            //     pcmData[i] = result[i] * Int16ToFloat32;
+            // }
+            int simdLength = length / Vector128<short>.Count;
+            for (int i = 0; i < simdLength; i++)
             {
-                pcmData[i] = result[i];
+                Vector128<short> inputVec = Vector128<short>.Zero;
+                for (int j = 0; j < Vector128<short>.Count; j++)
+                {
+                    inputVec = inputVec.WithElement(j, result[i * Vector128<short>.Count + j]);
+                }
+
+                Vector128<float> outputVec = Vector128<float>.Zero;
+                for (int j = 0; j < Vector128<float>.Count; j++)
+                {
+                    outputVec = outputVec.WithElement(j, inputVec.GetElement(j) * Int16ToFloat32);
+                }
+
+                for (int j = 0; j < Vector128<float>.Count; j++)
+                {
+                    pcmData[i * Vector128<float>.Count + j] = outputVec.GetElement(j);
+                }
+            }
+
+            // Handle remaining elements
+            for (int i = simdLength * Vector128<short>.Count; i < length; i++)
+            {
+                pcmData[i] = result[i] * Int16ToFloat32;
             }
         }
         else
         {
             //interleave to deinterleave
-            for (int i = 0; i < length; i++)
+            int samplesPerChannel = length / channel;
+            for (int channelIndex = 0; channelIndex < channel; channelIndex++)
             {
-                pcmData[i] = result[i * channel];
+                int channelStart = channelIndex * samplesPerChannel;
+                for (int i = 0; i < samplesPerChannel; i++)
+                {
+                    pcmData[channelStart + i] = result[i * channel] * Int16ToFloat32;
+                }
             }
         }
 
         CRuntime.free(result);
         return AudioClip.UnsafeCreate(pcmData, (uint)length, sampleRate);
     }
+
+    
 }
