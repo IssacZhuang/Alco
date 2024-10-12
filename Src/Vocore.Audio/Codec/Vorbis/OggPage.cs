fc85f7ca9db1d1dae3a2ad16b73017ea92a38ec8
zhuang jr <1095313400@qq.com>
2024-10-12 18:42:47 +0800

    wip: ogg decoder

--- Src/Vocore.Audio/Codec/Vorbis/OggPage.cs
+++ Src/Vocore.Audio/Codec/Vorbis/OggPage.cs
@@ -1,10 +1,26 @@
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace Vocore.Audio;
 
 [StructLayout(LayoutKind.Explicit)]
-public unsafe struct OggHeader
+public unsafe struct OggPage
 {
+    private static readonly byte[] Name = Encoding.ASCII.GetBytes("OggS");
+
+    public static bool IsOggHeader(byte* ptr)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (ptr[i] != Name[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     [FieldOffset(0)]
     public fixed byte CapturePattern[4];//"OggS" in ASCII
     [FieldOffset(4)]
--- Src/Vocore.Audio/Codec/Vorbis/VorbisDecoder.cs
+++ Src/Vocore.Audio/Codec/Vorbis/VorbisDecoder.cs
@@ -2,36 +2,21 @@ namespace Vocore.Audio;
 
 public static unsafe class VorbisDecoder
 {
-    public static float[] DecodeWaveAudioToFloat32(ReadOnlySpan<byte> data, out int channels, out int sampleRate)
+    public static float[] DecodeVorbisAudioToFloat32(ReadOnlySpan<byte> data, out int channels, out int sampleRate)
     {
         fixed (byte* ptr = data)
         {
             // ----------- Header -----------
 
             byte* p = ptr;
-            OggHeader header = *(OggHeader*)p;
-            p += sizeof(OggHeader);
+            OggPage header = *(OggPage*)p;
+            p += sizeof(OggPage);
 
             byte* pSegmentLengths = p;
             p += header.PageSegments;
 
 
-            // ----------- Metadata -----------
-
-            //skip comments
-            uint vendorStringLength = *(uint*)p;
-            p += sizeof(uint);
-            p += vendorStringLength;
-
-            uint commentsCount = *(uint*)p;
-            p += sizeof(uint);
-
-            for (int i = 0; i < commentsCount; i++)
-            {
-                uint commentLength = *(uint*)p;
-                p += sizeof(uint);
-                p += commentLength;
-            }
+            
 
             
         }
