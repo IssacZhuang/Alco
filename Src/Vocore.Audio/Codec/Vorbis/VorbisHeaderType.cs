b7dc3ba7690305365c651fd906fcbb35c45ee5a0
zhuang jr <1095313400@qq.com>
2024-10-21 19:17:24 +0800

    remove custom vorbis reader

--- Src/Vocore.Audio/Codec/Vorbis/VorbisHeaderType.cs
+++
@@ -1,9 +0,0 @@
-namespace Vocore.Audio;
-
-internal enum VorbisHeaderType : byte
-{
-    Identification = 1,
-    Comment = 3,
-    Setup = 5,
-    Audio = 0 // Audio packets do not have a specific type byte
-}
