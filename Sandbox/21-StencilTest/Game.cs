246dccc938984a802c408a366998430d8c99b1f6
zhuang jr <1095313400@qq.com>
2024-11-12 17:53:56 +0800

    feature: stencil test sandbox

---
+++ Sandbox/21-StencilTest/Game.cs
@@ -0,0 +1,22 @@
+using Vocore;
+using Vocore.Engine;
+using Vocore.Graphics;
+
+public class Game : GameEngine
+{
+    public Game(GameEngineSetting setting) : base(setting)
+    {
+        
+
+    }
+
+    override protected void OnUpdate(float delta)
+    {
+        
+    }
+
+    protected override void OnStop()
+    {
+
+    }
+}
