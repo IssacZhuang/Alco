5ae5a7da0afef60bc9866fd898a95d9ec82ec039
IssacZhuang <1095313400@qq.com>
2024-12-15 14:41:05 +0800

    opt: use frozen dictionary to do type cast

--- Src/Vocore.Graphics/Common/UtilsCast.cs
+++ Src/Vocore.Graphics/Common/UtilsCast.cs
@@ -1,8 +1,10 @@
+using System.Collections.Frozen;
+
 namespace Vocore.Graphics;
 
 internal static class UtilsCast
 {
-    public static Func<TA, TB> GenerateCastFunc<TA, TB>(Dictionary<TA, TB> castTable) where TA : notnull where TB : notnull
+    public static Func<TA, TB> GenerateCastFunc<TA, TB>(FrozenDictionary<TA, TB> castTable) where TA : notnull where TB : notnull
     {
         return (a) =>
         {
@@ -15,15 +17,17 @@ internal static class UtilsCast
     }
     
     // Tuple
-    public static void GenerateCastTable<TA, TB>(IEnumerable<Tuple<TA, TB>> castList, out Dictionary<TA, TB> aToB, out Dictionary<TB, TA> bToA) where TA : notnull where TB : notnull
+    public static void GenerateCastTable<TA, TB>(IEnumerable<Tuple<TA, TB>> castList, out FrozenDictionary<TA, TB> aToB, out FrozenDictionary<TB, TA> bToA) where TA : notnull where TB : notnull
     {
-        aToB = new Dictionary<TA, TB>();
-        bToA = new Dictionary<TB, TA>();
+        Dictionary<TA, TB> tmpAToB = new Dictionary<TA, TB>();
+        Dictionary<TB, TA> tmpBToA = new Dictionary<TB, TA>();
         foreach (var (a, b) in castList)
         {
-            aToB.Add(a, b);
-            bToA.Add(b, a);
+            tmpAToB.Add(a, b);
+            tmpBToA.Add(b, a);
         }
+        aToB = tmpAToB.ToFrozenDictionary();
+        bToA = tmpBToA.ToFrozenDictionary();
     }
 
     public static Func<TA, TB> GenerateCastFunc<TA, TB>(IEnumerable<Tuple<TA, TB>> castList) where TA : notnull where TB : notnull
@@ -54,15 +58,17 @@ internal static class UtilsCast
     }
 
     //ValueTuple
-    public static void GenerateCastTable<TA, TB>(IEnumerable<ValueTuple<TA, TB>> castList, out Dictionary<TA, TB> aToB, out Dictionary<TB, TA> bToA) where TA : notnull where TB : notnull
+    public static void GenerateCastTable<TA, TB>(IEnumerable<ValueTuple<TA, TB>> castList, out FrozenDictionary<TA, TB> aToB, out FrozenDictionary<TB, TA> bToA) where TA : notnull where TB : notnull
     {
-        aToB = new Dictionary<TA, TB>();
-        bToA = new Dictionary<TB, TA>();
+        Dictionary<TA, TB> tmpAToB = new Dictionary<TA, TB>();
+        Dictionary<TB, TA> tmpBToA = new Dictionary<TB, TA>();
         foreach (var (a, b) in castList)
         {
-            aToB.Add(a, b);
-            bToA.Add(b, a);
+            tmpAToB.Add(a, b);
+            tmpBToA.Add(b, a);
         }
+        aToB = tmpAToB.ToFrozenDictionary();
+        bToA = tmpBToA.ToFrozenDictionary();
     }
 
     public static Func<TA, TB> GenerateCastFunc<TA, TB>(IEnumerable<ValueTuple<TA, TB>> castList) where TA : notnull where TB : notnull
