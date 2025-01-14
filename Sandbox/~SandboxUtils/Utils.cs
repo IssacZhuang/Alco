d286a18ab7c2ddca6a36854c8a6b5a4ed31fc811
IssacZhuang <1095313400@qq.com>
2025-01-14 23:31:00 +0800

    feature: sandbox uitls

---
+++ Sandbox/~SandboxUtils/Utils.cs
@@ -0,0 +1,24 @@
+﻿namespace SandboxUtils;
+
+public static class Uitls
+{
+    public static string GetSolutionFolder()
+    {
+        string? current = Directory.GetCurrentDirectory();
+        while (current != null)
+        {
+            string[] files = Directory.GetFiles(current, "*.sln");
+            if (files.Length > 0)
+            {
+                return current;
+            }
+            current = Directory.GetParent(current)?.FullName;
+        }
+        throw new Exception("Solution file not found");
+    }
+
+    public static string GetBuiltInAssetsFolder()
+    {
+        return Path.Combine(GetSolutionFolder(), "Src", "Vocore.Engine", "Assets");
+    }
+}
