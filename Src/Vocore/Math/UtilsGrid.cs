dffdfdf55854ccd2c2e1f8aca454715756f74b52
IssacZhuang <1095313400@qq.com>
2025-01-15 23:37:10 +0800

    feature: grid utils

---
+++ Src/Vocore/Math/UtilsGrid.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+
+using static Vocore.math;
+
+namespace Vocore;
+
+public static class UtilsGrid
+{
+    public const int RadialPatternCount = 10000;
+    public const int RadialPatternRadius = 60;
+    public static readonly IReadOnlyList<int2> RadialPattern;
+    private static readonly float[] RadialPatternRadii;
+
+    static UtilsGrid()
+    {
+        int2[] radialPattern = new int2[RadialPatternCount];
+        float[] radialPatternRadii = new float[RadialPatternCount];
+
+        List<int2> list = new List<int2>();
+        for (int i = -RadialPatternRadius; i < RadialPatternRadius; i++)
+        {
+            for (int j = -RadialPatternRadius; j < RadialPatternRadius; j++)
+            {
+                list.Add(new int2(i, j));
+            }
+        }
+
+        list.Sort((int2 A, int2 B) =>
+        {
+            float squaredDistanceA = A.x * (float)A.x + A.y * (float)A.y;
+            float squaredDistanceB = B.x * (float)B.x + B.y * (float)B.y;
+            if (squaredDistanceA < squaredDistanceB)
+            {
+                return -1;
+            }
+            return (squaredDistanceA != squaredDistanceB) ? 1 : 0;
+        });
+
+        for (int i = 0; i < RadialPatternCount; i++)
+        {
+            radialPattern[i] = list[i];
+            int2 vector2Int = list[i];
+            radialPatternRadii[i] = sqrt(vector2Int.x * (float)vector2Int.x + vector2Int.y * (float)vector2Int.y);
+        }
+
+        RadialPattern = radialPattern;
+        RadialPatternRadii = radialPatternRadii;
+    }
+
+    /// <summary>
+    /// Get the number of cells in a radius
+    /// </summary>
+    /// <param name="radius">The radius</param>
+    /// <returns>The number of cells in the radius</returns>
+    public static int GetCellCountInRadius(float radius)
+    {
+        float searchRadius = radius + float.Epsilon;
+        int searchIndex = Array.BinarySearch(RadialPatternRadii, searchRadius);
+        if (searchIndex < 0)
+        {
+            return ~searchIndex;
+        }
+        for (int i = searchIndex; i < RadialPatternCount; i++)
+        {
+            if (RadialPatternRadii[i] > searchRadius)
+            {
+                return i;
+            }
+        }
+        return RadialPatternCount;
+    }
+
+    /// <summary>
+    /// Get the cells positions in a radius
+    /// </summary>
+    /// <param name="radius">The radius</param>
+    /// <param name="cells">The list of cells</param>
+    public static void GetCellsInRadius(List<int2> cells, float radius)
+    {
+        cells.Clear();
+        int count = GetCellCountInRadius(radius);
+        for (int i = 0; i < count; i++)
+        {
+            cells.Add(RadialPattern[i]);
+        }
+    }
+}
+
