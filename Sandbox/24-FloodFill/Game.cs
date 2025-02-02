2def5e046325df212491c53512e6ea3b54fe1351
IssacZhuang <1095313400@qq.com>
2025-02-03 02:58:37 +0800

    feature: tile light map

--- Sandbox/24-FloodFill/Game.cs
+++ Sandbox/24-FloodFill/Game.cs
@@ -15,17 +15,12 @@ public class Game : GameEngine
     }
 
     private readonly uint2 _size = new uint2(65, 65);
-    private readonly RenderTexture _lightMap1;
-    private readonly RenderTexture _lightMap2;
-    private readonly DoubleBuffer<RenderTexture> _lightMap;
-    private readonly BitmapFloat16RGBA _lightMapCPU;
     private readonly MaterialRenderer _materialRenderer;
     private readonly Camera2D _camera;
     private readonly Material _material;
-    private readonly ComputeMaterial _computeClearTexture;
-    private readonly ComputeMaterial _computeFloodFill;
+    private readonly TileMapHeightBuffer _heightBuffer;
+    private readonly TileLightMap _tileLightMap;
 
-    private readonly GPUCommandBuffer _command;
     private readonly GraphicsValueBuffer<Data> _dataBuffer;
 
     private float _intensity = 1;
@@ -34,25 +29,13 @@ public class Game : GameEngine
     public Game(GameEngineSetting setting) : base(setting)
 
     {
-        _command = GraphicsDevice.CreateCommandBuffer();
-        _lightMap1 = Rendering.CreateRenderTexture(Rendering.PrefferedLightMapPass, _size.x, _size.y, FilterMode.Linear, "light_map_1");
-        _lightMap2 = Rendering.CreateRenderTexture(Rendering.PrefferedLightMapPass, _size.x, _size.y, FilterMode.Linear, "light_map_2");
-
-
-        _lightMap = new DoubleBuffer<RenderTexture>(_lightMap1, _lightMap2);
-
-        _lightMapCPU = new BitmapFloat16RGBA(_size.x, _size.y);
-
-        //set center pixel to 1
-        _lightMapCPU[(int)_size.x / 2, (int)_size.y / 2] = new Half4(_intensity, _intensity, _intensity, 1);
-
         Material blitMaterial = Rendering.CreateGraphicsMaterial(Assets.Load<Shader>("InverserGamma.hlsl"));
 
         _camera = Rendering.CreateCamera2D(MainWindow.Size, 1000);
         _materialRenderer = Rendering.CreateMaterialRenderer();
         _material = blitMaterial.CreateInstance();
         _material.SetBuffer(ShaderResourceId.Camera, _camera);
-        _material.SetRenderTexture(ShaderResourceId.Texture, _lightMap1);
+
 
         _dataBuffer = Rendering.CreateGraphicsValueBuffer<Data>("data_buffer");
         _dataBuffer.Value.attenuationSide = 0.1f;
@@ -60,15 +43,13 @@ public class Game : GameEngine
         _dataBuffer.UpdateBuffer();
 
 
+        _heightBuffer = Rendering.CreateTileMapHeightBuffer((int)_size.x, (int)_size.y, "height_buffer");
 
+        ComputeMaterial computeMaterial = Rendering.CreateComputeMaterial(BuiltInAssets.Shader_FloodFillLighting);
 
+        _tileLightMap = Rendering.CreateTileLightMap(_heightBuffer, computeMaterial, (int)_size.x, (int)_size.y, "tile_light_map");
 
-        Shader shaderClearTexture = BuiltInAssets.Shader_ClearTexture;
-        _computeClearTexture = Rendering.CreateComputeMaterial(shaderClearTexture);
-
-        Shader shaderFloodFill = BuiltInAssets.Shader_FloodFillLighting;
-        _computeFloodFill = Rendering.CreateComputeMaterial(shaderFloodFill);
-        _computeFloodFill.SetBuffer(ShaderResourceId.Data, _dataBuffer);
+        _material.SetRenderTexture(ShaderResourceId.Texture, _tileLightMap.LightMap);
     }
 
 
@@ -113,9 +94,9 @@ public class Game : GameEngine
         _camera.UpdateMatrixToGPU();
 
         Transform2D transform = Transform2D.Identity;
-        float scale = MainWindow.Width / _lightMap1.Width;
-        scale = math.min(scale, MainWindow.Height / _lightMap1.Height);
-        transform.scale = new Vector2(_lightMap1.Width * scale, _lightMap1.Height * scale);
+        float scale = MainWindow.Width / _tileLightMap.Width;
+        scale = math.min(scale, MainWindow.Height / _tileLightMap.Height);
+        transform.scale = new Vector2(_tileLightMap.Width * scale, _tileLightMap.Height * scale);
 
         SpriteConstant constant = new SpriteConstant
         {
@@ -124,20 +105,8 @@ public class Game : GameEngine
             UvRect = new Rect(0, 0, 1, 1)
         };
 
-        _lightMapCPU[(int)_size.x / 2, (int)_size.y / 2] = new Half4(_intensity, _intensity, _intensity, 1);
-        _lightMap1.ColorTextures[0].SetPixels(_lightMapCPU);
-        _lightMap.Reset();
-
-        _command.Begin();
-        for (int i = 0; i < _iterations; i++)
-        {
-            _computeFloodFill.SetRenderTexture(ShaderResourceId.FrontBuffer, _lightMap.Front);
-            _computeFloodFill.SetRenderTexture(ShaderResourceId.BackBuffer, _lightMap.Back);
-            _lightMap.Swap();
-            _computeFloodFill.Dispatch(_command, 5, 5, 1);
-        }
-        _command.End();
-        GraphicsDevice.Submit(_command);
+        _tileLightMap.SetLight((int)_size.x / 2, (int)_size.y / 2, new Half4(_intensity, _intensity, _intensity, 1));
+        _tileLightMap.Render();
 
         //draw atlas texture
         _materialRenderer.Begin(MainRenderTarget.FrameBuffer);
--- Src/Alco.Rendering/TileMap/TileLightMap.cs
+++ Src/Alco.Rendering/TileMap/TileLightMap.cs
@@ -1,21 +1,54 @@
 using System.Numerics;
+using Alco.Graphics;
 
 namespace Alco.Rendering;
 
 public class TileLightMap : AutoDisposable
 {
+    private struct Data
+    {
+        public float attenuationSide;
+        public float attenuationCorner;
+    }
+    private readonly GPUDevice _device;
     private readonly RenderTexture _lightMapFront;
     private readonly RenderTexture _lightMapBack;
-    private readonly DoubleBuffer<RenderTexture> _lightMap;
+    private readonly DoubleBuffer<RenderTexture> _lightMaps;
     private readonly BitmapFloat16RGBA _lightMapCPU;
     private readonly TileMapHeightBuffer _heightBuffer;
-    private readonly ComputeMaterial _computeDispatcher;
+    private readonly GraphicsValueBuffer<Data> _dataBuffer;
+    private readonly ComputeMaterial _material;
+    private readonly GPUCommandBuffer _command;
+    private bool _isDirty = true;
 
-    private uint _shaderid_front;
-    private uint _shaderid_back;
+    private uint _shaderId_front;
+    private uint _shaderId_back;
+
+
+    public int Iteration { get; set; } = 32;
+    public float AttenuationSide
+    {
+        get => _dataBuffer.Value.attenuationSide;
+        set {
+            _dataBuffer.Value.attenuationSide = value;
+            _isDirty = true;
+        }
+    }
+
+    public float AttenuationCorner
+    {
+        get => _dataBuffer.Value.attenuationCorner;
+        set {
+            _dataBuffer.Value.attenuationCorner = value;
+            _isDirty = true;
+        }
+    }
+
+    public RenderTexture LightMap => _lightMaps.Front;
 
     public int Width => _lightMapCPU.Width;
     public int Height => _lightMapCPU.Height;
+    public string Name { get; }
 
 
     internal TileLightMap(
@@ -23,18 +56,30 @@ public class TileLightMap : AutoDisposable
         TileMapHeightBuffer heightBuffer,
         ComputeMaterial computeDispatcher,
         int width,
-        int height
+        int height,
+        string name = "tile_light_map"
         )
     {
         _lightMapFront = renderingSystem.CreateRenderTexture(renderingSystem.PrefferedLightMapPass, (uint)width, (uint)height, "tile_light_map");
         _lightMapBack = renderingSystem.CreateRenderTexture(renderingSystem.PrefferedLightMapPass, (uint)width, (uint)height, "tile_light_map");
-        _lightMap = new DoubleBuffer<RenderTexture>(_lightMapFront, _lightMapBack);
+        _lightMaps = new DoubleBuffer<RenderTexture>(_lightMapFront, _lightMapBack);
         _lightMapCPU = new BitmapFloat16RGBA(width, height);
         _heightBuffer = heightBuffer;
-        _computeDispatcher = computeDispatcher.CreateInstance();
+        _material = computeDispatcher.CreateInstance();
 
-        _shaderid_front = _computeDispatcher.GetResourceId(ShaderResourceId.FrontBuffer);
-        _shaderid_back = _computeDispatcher.GetResourceId(ShaderResourceId.BackBuffer);
+        _device = renderingSystem.GraphicsDevice;
+        _command = _device.CreateCommandBuffer();
+
+        _dataBuffer = renderingSystem.CreateGraphicsValueBuffer<Data>();
+        _dataBuffer.Value.attenuationSide = 0.1f;
+        _dataBuffer.Value.attenuationCorner = 0.14141414f;
+        _dataBuffer.UpdateBuffer();
+        _material.SetBuffer(ShaderResourceId.Data, _dataBuffer);
+
+        _shaderId_front = _material.GetResourceId(ShaderResourceId.FrontBuffer);
+        _shaderId_back = _material.GetResourceId(ShaderResourceId.BackBuffer);
+
+        Name = name;
 
     }
 
@@ -48,6 +93,31 @@ public class TileLightMap : AutoDisposable
     public void AddLight(int x, int y, Half4 light)
     {
         _lightMapCPU[x, y] = _lightMapCPU[x, y] + light;
+    }
+
+    public void SetLight(int x, int y, Half4 light)
+    {
+        _lightMapCPU[x, y] = light;
+    }
+
+    public void Render()
+    {
+        if(_isDirty){
+            _dataBuffer.UpdateBuffer();
+            _isDirty = false;
+        }
+        _lightMaps.Reset();
+        _lightMaps.Front.ColorTextures[0].SetPixels(_lightMapCPU);
+        _command.Begin();
+        for (int i = 0; i < Iteration; i++)
+        {
+            _material.SetRenderTexture(_shaderId_front, _lightMaps.Front);
+            _material.SetRenderTexture(_shaderId_back, _lightMaps.Back);
+            _material.Dispatch(_command, 5, 5, 1);
+            _lightMaps.Swap();
+        }
+        _command.End();
+        _device.Submit(_command);
     }
 
 
