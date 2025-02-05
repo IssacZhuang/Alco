aaa0507571e6fc15a697d4b6c446c4e91957cb65
IssacZhuang <1095313400@qq.com>
2025-02-05 20:04:14 +0800

    feature: editor templete

---
+++ Editor/Alco.Editor/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Alco.Editor.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ Editor/Alco.Editor/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Alco.Editor.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
