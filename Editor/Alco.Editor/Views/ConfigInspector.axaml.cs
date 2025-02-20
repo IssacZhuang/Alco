2b5325d61b4758d1ea079d7a3ef1e625d84ce835
zhuang jr <1095313400@qq.com>
2025-02-20 10:46:38 +0800

    fix: config inspector view model in design mode

--- Editor/Alco.Editor/Views/ConfigInspector.axaml.cs
+++ Editor/Alco.Editor/Views/ConfigInspector.axaml.cs
@@ -2,23 +2,32 @@ using System;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Diagnostics;
 
 namespace Alco.Editor.Views;
 
 public partial class ConfigInspector : UserControl
 {
 
-    public ViewModels.ConfigInspector ViewModel => DataContext as ViewModels.ConfigInspector ?? throw new InvalidOperationException("DataContext is not a ViewModels.ExplorerPage");
+    public ViewModels.ConfigInspector ViewModel => DataContext as ViewModels.ConfigInspector ?? throw new InvalidOperationException("DataContext is not a ViewModels.ConfigInspector");
 
     public ConfigInspector()
     {
         InitializeComponent();
     }
 
+    protected override void OnDataContextChanged(EventArgs e)
+    {
+        base.OnDataContextChanged(e);
+
+    }
+
     protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
     {
         base.OnAttachedToVisualTree(e);
+
         TextJsonPreview.Text = ViewModel.SerializedJson;
 
+        
     }
 }
