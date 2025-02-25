758c64958f6c4555c726154a08fd8e8805dcfeaa
zhuang jr <1095313400@qq.com>
2025-02-25 17:40:23 +0800

    feature: access type info

---
+++ Src/Alco/Serialization/MemberAccessor/AccessMemberInfo.cs
@@ -0,0 +1,139 @@
+using System;
+using System.Globalization;
+using System.Reflection;
+
+namespace Alco;
+
+/// <summary>
+/// Base class for accessing members (properties and fields) of objects through reflection.
+/// Provides a unified interface for getting and setting values regardless of member type.
+/// </summary>
+public abstract class AccessMemberInfo
+{
+    /// <summary>
+    /// Gets the type of the property or field.
+    /// </summary>
+    public Type PropertyType { get; }
+
+    /// <summary>
+    /// Gets the name of the member.
+    /// </summary>
+    public string Name { get; }
+
+    /// <summary>
+    /// Gets a function that retrieves the value of this member from an object.
+    /// </summary>
+    protected abstract Func<object, object?> MethodGet { get; }
+
+    /// <summary>
+    /// Gets an action that sets the value of this member on an object.
+    /// </summary>
+    protected abstract Action<object, object?> MethodSet { get; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AccessMemberInfo"/> class.
+    /// </summary>
+    /// <param name="propertyType">The type of the property or field.</param>
+    /// <param name="name">The name of the member.</param>
+    internal AccessMemberInfo(Type propertyType, string name)
+    {
+        PropertyType = propertyType;
+        Name = name;
+    }
+
+    /// <summary>
+    /// Gets the value of the member as the specified type. 
+    /// </summary>
+    /// <param name="obj">The object to get the value from.</param>
+    /// <typeparam name="T">The type to get the value as.</typeparam>
+    /// <returns>The value of the member as the specified type, or null if the value is not of the specified type.</returns>
+    public T? GetValue<T>(object obj)
+    {
+        if (MethodGet is Func<object, T> getter)
+        {
+            return getter(obj);
+        }
+        return (T?)MethodGet(obj);
+    }
+
+    /// <summary>
+    /// Sets the value of the member to the specified value.
+    /// </summary>
+    /// <param name="obj">The object to set the value on.</param>
+    /// <param name="value">The value to set the member to.</param>
+    public void SetValue<T>(object obj, T value)
+    {
+        if (MethodSet is Action<object, T> setter)
+        {
+            setter(obj, value);
+        }
+    }
+
+    /// <summary>
+    /// Creates an <see cref="AccessMemberInfo"/> instance for a property.
+    /// </summary>
+    /// <param name="propertyInfo">The property information.</param>
+    /// <param name="accessor">The member accessor to use for creating getters and setters.</param>
+    /// <returns>An <see cref="AccessMemberInfo"/> instance for the property.</returns>
+    public static AccessMemberInfo Create(PropertyInfo propertyInfo, MemberAccessor accessor)
+    {
+        return (AccessMemberInfo)typeof(AccessPropertyInfo<>).
+            MakeGenericType(propertyInfo.PropertyType).
+            CreateInstanceNoWrapExceptions([typeof(PropertyInfo), typeof(MemberAccessor)], [propertyInfo, accessor])!;
+    }
+
+    /// <summary>
+    /// Creates an <see cref="AccessMemberInfo"/> instance for a field.
+    /// </summary>
+    /// <param name="fieldInfo">The field information.</param>
+    /// <param name="accessor">The member accessor to use for creating getters and setters.</param>
+    /// <returns>An <see cref="AccessMemberInfo"/> instance for the field.</returns>
+    public static AccessMemberInfo Create(FieldInfo fieldInfo, MemberAccessor accessor)
+    {
+        return (AccessMemberInfo)typeof(AccessFieldInfo<>).
+            MakeGenericType(fieldInfo.FieldType).
+            CreateInstanceNoWrapExceptions([typeof(FieldInfo), typeof(MemberAccessor)], [fieldInfo, accessor])!;
+    }
+
+}
+
+
+internal sealed class AccessPropertyInfo<T> : AccessMemberInfo
+{
+
+    protected override Func<object, object?> MethodGet { get; }
+    protected override Action<object, object?> MethodSet { get; }
+
+    public AccessPropertyInfo(PropertyInfo propertyInfo, MemberAccessor accessor) :
+    base(propertyInfo.PropertyType, propertyInfo.Name)
+    {
+
+
+        var typedGetter = accessor.CreatePropertyGetter<T>(propertyInfo);
+        MethodGet = typedGetter is Func<object, object?> getter ? getter : obj => typedGetter(obj);
+
+        var typedSetter = accessor.CreatePropertySetter<T>(propertyInfo);
+        MethodSet = typedSetter is Action<object, object?> setter ? setter : (obj, value) => typedSetter(obj, (T)value!);
+    }
+}
+
+/// <summary>
+/// Provides access to a field with strongly-typed getters and setters.
+/// </summary>
+/// <typeparam name="T">The type of the field.</typeparam>
+internal sealed class AccessFieldInfo<T> : AccessMemberInfo
+{
+    protected override Func<object, object?> MethodGet { get; }
+    protected override Action<object, object?> MethodSet { get; }
+
+    public AccessFieldInfo(FieldInfo fieldInfo, MemberAccessor accessor) :
+    base(fieldInfo.FieldType, fieldInfo.Name)
+    {
+        var typedGetter = accessor.CreateFieldGetter<T>(fieldInfo);
+        MethodGet = typedGetter is Func<object, object?> getter ? getter : obj => typedGetter(obj);
+
+        var typedSetter = accessor.CreateFieldSetter<T>(fieldInfo);
+        MethodSet = typedSetter is Action<object, object?> setter ? setter : (obj, value) => typedSetter(obj, (T)value!);
+    }
+}
+
---
+++ Src/Alco/Serialization/MemberAccessor/AccessTypeInfo.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace Alco;
+
+/// <summary>
+/// Provides type information for accessing public properties and fields of a type.
+/// This class is used for reflection-based member access and serialization.
+/// </summary>
+public class AccessTypeInfo
+{
+    /// <summary>
+    /// Binding flags used to retrieve all instance members of a type,
+    /// including both public and non-public members that are declared in the type itself.
+    /// </summary>
+    private const BindingFlags AllInstanceMembers =
+            BindingFlags.Instance |
+            BindingFlags.Public |
+            BindingFlags.NonPublic |
+            BindingFlags.DeclaredOnly;
+
+    /// <summary>
+    /// Gets an array of <see cref="AccessMemberInfo"/> objects representing 
+    /// all accessible members (public properties and fields) of the type.
+    /// </summary>
+    public AccessMemberInfo[] Members { get; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AccessTypeInfo"/> class.
+    /// </summary>
+    /// <param name="type">The type to analyze for accessible members.</param>
+    /// <param name="memberAccessor">The member accessor used to create access information for each member.</param>
+    public AccessTypeInfo(Type type, MemberAccessor memberAccessor)
+    {
+        //get all public properties and fields
+        var fields = type.GetFields(AllInstanceMembers);
+        var properties = type.GetProperties(AllInstanceMembers);
+        List<AccessMemberInfo> accessMembers = new();
+
+        foreach (var property in properties)
+        {
+            if (property.GetMethod?.IsPublic == true ||
+                property.SetMethod?.IsPublic == true)
+            {
+                accessMembers.Add(AccessMemberInfo.Create(property, memberAccessor));
+            }
+        }
+
+        foreach (var field in fields)
+        {
+            if (field.IsPublic)
+            {
+                accessMembers.Add(AccessMemberInfo.Create(field, memberAccessor));
+            }
+        }
+
+        Members = accessMembers.ToArray();
+    }
+}
+
