f678afe8dbf94a6f69585b0e9af0d02c5ba3843c
IssacZhuang <1095313400@qq.com>
2025-02-27 20:54:32 +0800

    opt: vector2 property edit

--- Editor/Alco.Editor/ViewModels/PropertyVector2Editor.cs
+++ Editor/Alco.Editor/ViewModels/PropertyVector2Editor.cs
@@ -39,98 +39,79 @@ public class PropertyVector2Editor : PropertyEditor
         return control;
     }
 
+    private T GetVector<T>() where T : struct
+    {
+        return MemberInfo.GetValue<T>(Target);
+    }
+
+    private void SetVector<T>(T value) where T : struct
+    {
+        MemberInfo.SetValue(Target, value);
+    }
+
     private decimal GetValueX()
     {
-        if (MemberInfo.MemberType == typeof(Vector2))
+        return MemberInfo.MemberType switch
         {
-            return (decimal)MemberInfo.GetValue<Vector2>(Target).X;
-        }
-        else if (MemberInfo.MemberType == typeof(int2))
-        {
-            return (decimal)MemberInfo.GetValue<int2>(Target).X;
-        }
-        else if (MemberInfo.MemberType == typeof(uint2))
-        {
-            return (decimal)MemberInfo.GetValue<uint2>(Target).X;
-        }
-        else if (MemberInfo.MemberType == typeof(Half2))
-        {
-            return (decimal)MemberInfo.GetValue<Half2>(Target).X;
-        }
-        throw new InvalidOperationException($"Unsupported type: {MemberInfo.MemberType}");
+            Type t when t == typeof(Vector2) => (decimal)GetVector<Vector2>().X,
+            Type t when t == typeof(int2) => (decimal)GetVector<int2>().X,
+            Type t when t == typeof(uint2) => (decimal)GetVector<uint2>().X,
+            Type t when t == typeof(Half2) => (decimal)GetVector<Half2>().X,
+            _ => throw new InvalidOperationException($"Unsupported type: {MemberInfo.MemberType}")
+        };
     }
 
     private decimal GetValueY()
     {
-        if (MemberInfo.MemberType == typeof(Vector2))
+        return MemberInfo.MemberType switch
         {
-            return (decimal)MemberInfo.GetValue<Vector2>(Target).Y;
-        }
-        else if (MemberInfo.MemberType == typeof(int2))
-        {
-            return (decimal)MemberInfo.GetValue<int2>(Target).Y;
-        }
-        else if (MemberInfo.MemberType == typeof(uint2))
-        {
-            return (decimal)MemberInfo.GetValue<uint2>(Target).Y;
-        }
-        else if (MemberInfo.MemberType == typeof(Half2))
-        {
-            return (decimal)MemberInfo.GetValue<Half2>(Target).Y;
-        }
-        throw new InvalidOperationException($"Unsupported type: {MemberInfo.MemberType}");
+            Type t when t == typeof(Vector2) => (decimal)GetVector<Vector2>().Y,
+            Type t when t == typeof(int2) => (decimal)GetVector<int2>().Y,
+            Type t when t == typeof(uint2) => (decimal)GetVector<uint2>().Y,
+            Type t when t == typeof(Half2) => (decimal)GetVector<Half2>().Y,
+            _ => throw new InvalidOperationException($"Unsupported type: {MemberInfo.MemberType}")
+        };
     }
 
     private void SetValueX(decimal value)
     {
-        if (MemberInfo.MemberType == typeof(Vector2))
+        switch (MemberInfo.MemberType)
         {
-            Vector2 vector2 = MemberInfo.GetValue<Vector2>(Target);
-            MemberInfo.SetValue(Target, new Vector2((float)value, vector2.Y));
+            case Type t when t == typeof(Vector2):
+                SetVector(new Vector2((float)value, GetVector<Vector2>().Y));
+                break;
+            case Type t when t == typeof(int2):
+                SetVector(new int2((int)value, GetVector<int2>().Y));
+                break;
+            case Type t when t == typeof(uint2):
+                SetVector(new uint2((uint)value, GetVector<uint2>().Y));
+                break;
+            case Type t when t == typeof(Half2):
+                SetVector(new Half2((Half)value, GetVector<Half2>().Y));
+                break;
+            default:
+                throw new InvalidOperationException($"Unsupported type: {MemberInfo.MemberType}");
         }
-        else if (MemberInfo.MemberType == typeof(int2))
-        {
-            int2 int2 = MemberInfo.GetValue<int2>(Target);
-            MemberInfo.SetValue(Target, new int2((int)value, int2.Y));
-        }
-        else if (MemberInfo.MemberType == typeof(uint2))
-        {
-            uint2 uint2 = MemberInfo.GetValue<uint2>(Target);
-            MemberInfo.SetValue(Target, new uint2((uint)value, uint2.Y));
-        }
-        else if (MemberInfo.MemberType == typeof(Half2))    
-        {
-            Half2 half2 = MemberInfo.GetValue<Half2>(Target);
-            MemberInfo.SetValue(Target, new Half2((Half)value, half2.Y));
-        }
-        throw new InvalidOperationException($"Unsupported type: {MemberInfo.MemberType}");
     }
 
     private void SetValueY(decimal value)
     {
-        if (MemberInfo.MemberType == typeof(Vector2))
+        switch (MemberInfo.MemberType)
         {
-            Vector2 vector2 = MemberInfo.GetValue<Vector2>(Target);
-            MemberInfo.SetValue(Target, new Vector2(vector2.X, (float)value));
+            case Type t when t == typeof(Vector2):
+                SetVector(new Vector2(GetVector<Vector2>().X, (float)value));
+                break;
+            case Type t when t == typeof(int2):
+                SetVector(new int2(GetVector<int2>().X, (int)value));
+                break;
+            case Type t when t == typeof(uint2):
+                SetVector(new uint2(GetVector<uint2>().X, (uint)value));
+                break;
+            case Type t when t == typeof(Half2):
+                SetVector(new Half2(GetVector<Half2>().X, (Half)value));
+                break;
+            default:
+                throw new InvalidOperationException($"Unsupported type: {MemberInfo.MemberType}");
         }
-        else if (MemberInfo.MemberType == typeof(int2))
-        {
-            int2 int2 = MemberInfo.GetValue<int2>(Target);
-            MemberInfo.SetValue(Target, new int2(int2.X, (int)value));
-        }
-        else if (MemberInfo.MemberType == typeof(uint2))
-        {
-            uint2 uint2 = MemberInfo.GetValue<uint2>(Target);
-            MemberInfo.SetValue(Target, new uint2(uint2.X, (uint)value));
-        }
-        else if (MemberInfo.MemberType == typeof(Half2))
-        {
-            Half2 half2 = MemberInfo.GetValue<Half2>(Target);
-            MemberInfo.SetValue(Target, new Half2(half2.X, (Half)value));
-        }
-        throw new InvalidOperationException($"Unsupported type: {MemberInfo.MemberType}");
     }
-    
-    
-
 }
