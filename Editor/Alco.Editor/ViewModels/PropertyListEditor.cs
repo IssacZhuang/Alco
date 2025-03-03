671e76aa5d80598dfc543fad301a1a8e438af8f8
zhuang jr <1095313400@qq.com>
2025-03-03 17:10:02 +0800

    wip: list editor

--- Editor/Alco.Editor/ViewModels/PropertyListEditor.cs
+++ Editor/Alco.Editor/ViewModels/PropertyListEditor.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
 using Alco.Editor.Attributes;
 using Avalonia.Controls;
 
 namespace Alco.Editor.ViewModels;
 
-public class PropertyListEditor : PropertyEditor
+public abstract class PropertyListEditor : PropertyEditor
 {
     public PropertyListEditor(object target, AccessMemberInfo memberInfo) : base(target, memberInfo)
     {
@@ -17,4 +20,56 @@ public class PropertyListEditor : PropertyEditor
             DataContext = this,
         };
     }
+
+    public abstract void Add();
+    public abstract void RemoveAt(int index);
+    public abstract void RemoveLast();
+
+    public static bool TryCreate(object list, [NotNullWhen(true)] out PropertyListEditor? editor)
+    {
+        //check if list is generic list
+        if (!UtilsType.IsGenericList(list.GetType(), out Type? genericType))
+        {
+            editor = null;
+            return false;
+        }
+
+        Type editorType = typeof(PropertyListEditor<>).MakeGenericType(genericType);
+        editor = (PropertyListEditor)Activator.CreateInstance(editorType, list)!;
+        return true;
+    }
 }
+
+public class PropertyListEditor<T> : PropertyListEditor
+{
+    private static readonly bool IsClass = typeof(T).IsClass;
+    private readonly IList<T> _list;
+    private readonly List<PropertyEditor> _itemEditors = new();
+    public PropertyListEditor(IList<T> list) : base(list, AccessMemberInfo.Empty)
+    {
+        _list = list;
+    }
+
+    public override void Add()
+    {
+        if (IsClass)
+        {
+            _list.Add(Activator.CreateInstance<T>());
+        }
+        else
+        {
+            _list.Add(default!);
+        }
+    }
+
+    public override void RemoveAt(int index)
+    {
+        _list.RemoveAt(index);
+    }
+
+    public override void RemoveLast()
+    {
+        _list.RemoveAt(_list.Count - 1);
+    }
+}
+
