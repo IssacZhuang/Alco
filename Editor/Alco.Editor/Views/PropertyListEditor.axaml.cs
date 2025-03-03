39a6fb2c84cbd400d24d651bf453e599ec84e439
zhuang jr <1095313400@qq.com>
2025-03-03 18:26:28 +0800

    feature: list editot add remove

--- Editor/Alco.Editor/Views/PropertyListEditor.axaml.cs
+++ Editor/Alco.Editor/Views/PropertyListEditor.axaml.cs
@@ -1,6 +1,7 @@
 using System;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace Alco.Editor.Views;
@@ -27,6 +28,14 @@ public partial class PropertyListEditor : UserControl
 
     private void Setup(ViewModels.PropertyListEditor viewModel)
     {
+        RefreshList(viewModel);
+
+        BtnAdd.Click += Add;
+        BtnRemove.Click += RemoveLast;
+    }
+
+    private void RefreshList(ViewModels.PropertyListEditor viewModel)
+    {
         Root.Children.Clear();
         foreach (var item in viewModel.ItemEditors)
         {
@@ -37,5 +46,23 @@ public partial class PropertyListEditor : UserControl
     private void Clear()
     {
         Root.Children.Clear();
+    }
+
+    private void Add(object? sender, RoutedEventArgs e)
+    {
+        if (DataContext is ViewModels.PropertyListEditor viewModel)
+        {
+            viewModel.Add();
+            RefreshList(viewModel);
+        }
+    }
+
+    private void RemoveLast(object? sender, RoutedEventArgs e)
+    {
+        if (DataContext is ViewModels.PropertyListEditor viewModel)
+        {
+            viewModel.RemoveLast();
+            RefreshList(viewModel);
+        }
     }
 }
