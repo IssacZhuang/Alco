1bb4c835985b202b99a87925a6b7fc688549dd3b
zhuang jr <1095313400@qq.com>
2025-03-06 11:32:03 +0800

    feature: editor type database

---
+++ Editor/Alco.Editor/TypeDatabase.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Alco.IO;
+
+namespace Alco.Editor;
+
+public class TypeDatabase : IDisposable
+{
+    private readonly List<Type> _configTypes = new();
+    private bool _isConfigTypesDirty = false;
+
+    public IReadOnlyList<Type> ConfigTypes
+    {
+        get
+        {
+            if (_isConfigTypesDirty)
+            {
+                FetchAllConfigTypes();
+            }
+            return _configTypes;
+        }
+    }
+
+    public TypeDatabase()
+    {
+        FetchAllConfigTypes();
+        AppDomain.CurrentDomain.AssemblyLoad += OnAssemblyLoad;
+    }
+
+
+    private void OnAssemblyLoad(object? sender, AssemblyLoadEventArgs args)
+    {
+        _isConfigTypesDirty = true;
+    }
+
+
+
+    private void FetchAllConfigTypes()
+    {
+        _configTypes.Clear();
+        _configTypes.AddRange(
+            Assembly.GetExecutingAssembly()
+                .GetTypes()
+                .Where(t => t.IsSubclassOf(typeof(BaseConfig)) && !t.IsAbstract)
+        );
+    }
+
+    public void Dispose()
+    {
+        AppDomain.CurrentDomain.AssemblyLoad -= OnAssemblyLoad;
+    }
+}
+
