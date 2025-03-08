0feefa0fd6ced21dccb9b6230f2d8ca81243fe46
IssacZhuang <1095313400@qq.com>
2025-03-08 23:21:07 +0800

    feature: asset path auto complete

--- Editor/Alco.Editor/Views/PropertyStringEditor.axaml.cs
+++ Editor/Alco.Editor/Views/PropertyStringEditor.axaml.cs
@@ -3,9 +3,13 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Data;
 using Avalonia.Markup.Xaml;
+using Alco.IO;
 
 namespace Alco.Editor.Views;
 
+/// <summary>
+/// A user control for editing string properties with autocomplete functionality using asset file names
+/// </summary>
 public partial class PropertyStringEditor : UserControl
 {
     public PropertyStringEditor()
@@ -22,6 +26,10 @@ public partial class PropertyStringEditor : UserControl
         }
     }
 
+    /// <summary>
+    /// Sets up the AutoCompleteBox with asset file names and binds it to the view model
+    /// </summary>
+    /// <param name="viewModel">The view model to bind to</param>
     private void Setup(ViewModels.PropertyStringEditor viewModel)
     {
         ArgumentNullException.ThrowIfNull(viewModel);
@@ -32,16 +40,25 @@ public partial class PropertyStringEditor : UserControl
             return;
         }
 
-        InputText.Bind(TextBox.TextProperty, new Binding(nameof(viewModel.Value))
+        // Set up the AutoCompleteBox with asset file names
+        var engine = App.Main.Engine;
+        var assetSystem = engine.Assets;
+
+        // Set the items source to all file names in the asset system
+        InputText.ItemsSource = assetSystem.AllFileNames;
+
+        // Set filter mode to Contains so that typing any part of a filename will show suggestions
+        InputText.FilterMode = AutoCompleteFilterMode.Contains;
+
+        // Bind the selected value to the viewModel's Value property
+        InputText.Bind(AutoCompleteBox.TextProperty, new Binding(nameof(viewModel.Value))
         {
             Source = viewModel,
         });
 
         if (!memberInfo.CanWrite)
         {
-            InputText.IsReadOnly = true;
+            InputText.IsEnabled = false;
         }
-
-
     }
 }
