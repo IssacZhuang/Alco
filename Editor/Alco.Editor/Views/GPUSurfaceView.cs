ac86f2b4329c40d7621ad20acd10a77886970038
zhuang jr <1095313400@qq.com>
2025-03-26 17:34:24 +0800

    feature: gpu draw on avalonia control

--- Editor/Alco.Editor/Views/GPUSurfaceView.cs
+++ Editor/Alco.Editor/Views/GPUSurfaceView.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Alco.Engine;
+using Alco.Graphics;
 using Alco.Rendering;
 using Avalonia;
 using Avalonia.Controls;
@@ -8,32 +11,88 @@ using Avalonia.LogicalTree;
 using Avalonia.Platform;
 using Avalonia.Rendering.Composition;
 using Avalonia.VisualTree;
+using SDL3;
+using static SDL3.SDL3;
 
 namespace Alco.Editor.Views;
 
-public class GPUSurfaceView : NativeControlHost, IDisposable
+public unsafe partial class GPUSurfaceView : NativeControlHost, IDisposable
 {
+    private GPUDevice? _device;
+
     public IntPtr Handle { get; private set; }
 
-    public WindowRenderTarget? RenderTarget { get; private set; }
+    public GPUSwapchain? Swapchain { get; private set; }
 
-    
+    public GPUSurfaceView()
+    {
+    }
 
     protected override IPlatformHandle CreateNativeControlCore(IPlatformHandle parent)
     {
+        GameEngine engine = App.Main.Engine;
+        _device = engine.Rendering.GraphicsDevice;
+
         var handle = base.CreateNativeControlCore(parent);
         Handle = handle.Handle;
-        Sdl3Window window = Sdl3Window.CreateWin32Window(App.Main.Engine.GraphicsDevice, Handle);
-        uint width = (uint)Width;
-        uint height = (uint)Height;
-        GameEngine engine = App.Main.Engine;
+
+        Log.Info($"Native window handle created: {Handle:X}, IsVisible: {IsVisible}");
+
+        var bounds = Bounds;
+        uint width = math.max(1, (uint)bounds.Width);
+        uint height = math.max(1, (uint)bounds.Height);
         RenderingSystem renderingSystem = engine.Rendering;
-        RenderTarget = engine.CreateWindowRenderTarget(window, renderingSystem.PrefferedSDRPass, engine.BuiltInAssets.Shader_Blit);
+        GPUDevice device = renderingSystem.GraphicsDevice;
+
+        Swapchain = device.CreateSwapchain(
+            new SwapchainDescriptor(
+                SurfaceSource.CreateWin32Window(Handle, GetModuleHandleW(null)),
+                device.PrefferedSurfaceFomat,
+                null,
+                width,
+                height,
+                true));
+
+        Log.Success($"Swapchain created with size {width}x{height}");
+
+        // Initial clear will be done in OnLoaded
         return handle;
+    }
+
+    protected override void OnSizeChanged(SizeChangedEventArgs e)
+    {
+        base.OnSizeChanged(e);
+        if (Swapchain == null || _device == null) return;
+        var bounds = Bounds;
+        uint width = math.max(1, (uint)bounds.Width);
+        uint height = math.max(1, (uint)bounds.Height);
+        Swapchain.Resize(width, height);
+        Log.Success($"Resize to {width}x{height}");
+
+        Render();
+    }
+
+    private void Render()
+    {
+        if (Swapchain == null || _device == null) return;
+
+        Swapchain.Present();
+        GPUCommandBuffer commandBuffer = _device.CreateCommandBuffer();
+        commandBuffer.Begin();
+        commandBuffer.SetFrameBuffer(Swapchain.FrameBuffer);
+        commandBuffer.ClearColor(new ColorFloat(0, 1, 1, 1));
+        commandBuffer.End();
+        _device.Submit(commandBuffer);
+        Swapchain.Present();
+
+        Log.Success("Clear color completed");
     }
 
     public void Dispose()
     {
-        RenderTarget?.Dispose();
+        Swapchain?.Dispose();
     }
+
+    [LibraryImport("kernel32")]
+    private static partial nint GetModuleHandleW(ushort* lpModuleName);
 }
