9414e4d9f0f35214fd73db1912258b78e5bce0be
IssacZhuang <1095313400@qq.com>
2025-05-11 00:35:29 +0800

    update wgpu

--- Src/Alco.Graphics/WGPU/Binding/IChainType.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Amer Koleci and Contributors.
-// Licensed under the MIT License (MIT). See LICENSE in the repository root for more information.
-
-namespace WebGPU;
-
-public interface IChainType
-{
-    WGPUChainedStruct chain { get; }
-}
