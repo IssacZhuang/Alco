5ea7d90b6deb479e60e429a26d757688541864cc
zhuang jr <1095313400@qq.com>
2025-06-25 17:23:18 +0800

    remove old vulkan implementation

--- Src/Alco.Graphics/Vulkan/VulkanFramebufferBase.cs
+++
@@ -1,9 +0,0 @@
-using Vortice.Vulkan;
-
-namespace Alco.Graphics.Vulkan;
-
-
-internal abstract class VulkanFramebufferBase : GPUFrameBuffer
-{
-    public abstract VkRenderPassBeginInfo PassBegineInfo { get; }
-}
