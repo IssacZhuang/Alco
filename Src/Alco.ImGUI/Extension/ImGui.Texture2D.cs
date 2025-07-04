bef9d4b9ff038dd6981b9b9ae0b6c38d1e464753
issac <issac>
2025-07-04 18:34:56 +0800

    opt: imgui texture2d extension

--- Src/Alco.ImGUI/Extension/ImGui.Texture2D.cs
+++ Src/Alco.ImGUI/Extension/ImGui.Texture2D.cs
@@ -17,11 +17,16 @@ public static unsafe partial class ImGui
         Image(textureId, size);
     }
 
-    public static void Image(IntPtr textureId, Vector2 size, Rect uvRect)
+    public static void Image(Texture2D texture2D, Vector2 size, Rect uvRect)
     {
-        // Convert Rect to UV coordinates
-        // Rect.Origin represents the top-left corner (uv0)
-        // Rect.Origin + Rect.Size represents the bottom-right corner (uv1)
+        if (texture2D.IsDisposed || ImGUIRenderer.Instance == null)
+        {
+            Image((IntPtr)(-2), size);//it will render a white texture
+            return;
+        }
+
+        IntPtr textureId = ImGUIRenderer.Instance.AddTexture(texture2D);
+        
         Vector2 uv0 = uvRect.Origin;
         Vector2 uv1 = uvRect.Origin + uvRect.Size;
 
