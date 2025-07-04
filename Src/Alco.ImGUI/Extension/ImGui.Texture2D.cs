7820a66899e9b2e4ab8e3d317be831f4e3157fdd
issac <issac>
2025-07-04 18:26:11 +0800

    feature: uv rect support for imgui extension

--- Src/Alco.ImGUI/Extension/ImGui.Texture2D.cs
+++ Src/Alco.ImGUI/Extension/ImGui.Texture2D.cs
@@ -1,4 +1,3 @@
-
 using System.Numerics;
 using Alco.Rendering;
 
@@ -18,6 +17,17 @@ public static unsafe partial class ImGui
         Image(textureId, size);
     }
 
+    public static void Image(IntPtr textureId, Vector2 size, Rect uvRect)
+    {
+        // Convert Rect to UV coordinates
+        // Rect.Origin represents the top-left corner (uv0)
+        // Rect.Origin + Rect.Size represents the bottom-right corner (uv1)
+        Vector2 uv0 = uvRect.Origin;
+        Vector2 uv1 = uvRect.Origin + uvRect.Size;
+
+        Image(textureId, size, uv0, uv1);
+    }
+
     public static bool ImageButton(ReadOnlySpan<char> label, Texture2D texture2D, Vector2 size)
     {
         if (texture2D.IsDisposed || ImGUIRenderer.Instance == null)
@@ -29,4 +39,17 @@ public static unsafe partial class ImGui
         return ImageButton(label, textureId, size);
     }
 
+    public static bool ImageButton(ReadOnlySpan<char> label, Texture2D texture2D, Vector2 size, Rect uvRect)
+    {
+        Vector2 uv0 = uvRect.Origin;
+        Vector2 uv1 = uvRect.Origin + uvRect.Size;
+
+        if (texture2D.IsDisposed || ImGUIRenderer.Instance == null)
+        {
+            return ImageButton(label, (IntPtr)(-2), size, uv0, uv1);
+        }
+
+        IntPtr textureId = ImGUIRenderer.Instance.AddTexture(texture2D);        
+        return ImageButton(label, textureId, size, uv0, uv1);
+    }
 }
