c617bfb4c0d2c6daf3a418e3e8f906a64c302ae3
zhuang jr <1095313400@qq.com>
2025-07-04 15:50:25 +0800

    opt: remove auto compress in NativeArrayList

--- Src/Alco/Collection/Unsafe/NativeArrayList.cs
+++ Src/Alco/Collection/Unsafe/NativeArrayList.cs
@@ -7,6 +7,10 @@ using Alco;
 
 namespace Alco
 {
+    /// <summary>
+    /// A native array list implementation that provides dynamic array functionality with unsafe memory management.
+    /// </summary>
+    /// <typeparam name="T">The unmanaged type of elements in the array list.</typeparam>
     public unsafe struct NativeArrayList<T> : IDisposable where T : unmanaged
     {
         private const int DefaultSize = 4;
@@ -14,20 +18,29 @@ namespace Alco
         private int _length;
         private int _capacity;
         private bool _isDisposed;
-        private bool _autoCompress;
-        public bool AutoCompress { get => _autoCompress; set => _autoCompress = value; }
+
+        /// <summary>
+        /// Gets the current number of elements in the array list.
+        /// </summary>
         public readonly int Length
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             get => _length;
         }
 
+        /// <summary>
+        /// Gets a pointer to the underlying buffer.
+        /// </summary>
         public readonly unsafe T* UnsafePointer
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             get => (T*)_ptrBuffer;
         }
 
+        /// <summary>
+        /// Returns a span representing the entire array list.
+        /// </summary>
+        /// <returns>A span that represents the entire array list.</returns>
         public Span<T> AsSpan()
         {
             return new Span<T>((T*)_ptrBuffer, _length);
@@ -49,10 +62,24 @@ namespace Alco
             return new Span<T>((T*)_ptrBuffer + start, length);
         }
 
+        /// <summary>
+        /// Gets the size in bytes of each element.
+        /// </summary>
         public int Stride => sizeof(T);
+
+        /// <summary>
+        /// Gets a value indicating whether this collection is read-only.
+        /// </summary>
         public bool IsReadOnly => false;
+
+        /// <summary>
+        /// Gets a value indicating whether this instance has been disposed.
+        /// </summary>
         public bool IsDisposed => _isDisposed;
 
+        /// <summary>
+        /// Gets the current capacity of the array list.
+        /// </summary>
         public int Capacity
         {
             get
@@ -61,6 +88,9 @@ namespace Alco
             }
         }
 
+        /// <summary>
+        /// Gets a value indicating whether the array list has been initialized.
+        /// </summary>
         public bool Initialized
         {
             get
@@ -69,7 +99,12 @@ namespace Alco
             }
         }
 
-
+        /// <summary>
+        /// Gets or sets the element at the specified index.
+        /// </summary>
+        /// <param name="index">The zero-based index of the element to get or set.</param>
+        /// <returns>The element at the specified index.</returns>
+        /// <exception cref="IndexOutOfRangeException">Thrown when the index is out of range.</exception>
         public T this[int index]
         {
             get
@@ -90,6 +125,11 @@ namespace Alco
             }
         }
 
+        /// <summary>
+        /// Initializes a new instance of the NativeArrayList with the specified initial capacity.
+        /// </summary>
+        /// <param name="size">The initial capacity of the array list.</param>
+        /// <exception cref="EmptySizeException">Thrown when size is less than or equal to zero.</exception>
         public NativeArrayList(int size)
         {
             if (size <= 0) throw new EmptySizeException(nameof(size));
@@ -97,19 +137,12 @@ namespace Alco
             _length = 0;
             _capacity = size;
             _isDisposed = false;
-            _autoCompress = false;
         }
 
-        public NativeArrayList(int size, bool autoCompress)
-        {
-            if (size <= 0) throw new EmptySizeException(nameof(size));
-            _ptrBuffer = UtilsMemory.Alloc(sizeof(T) * size);
-            _length = 0;
-            _capacity = size;
-            _isDisposed = false;
-            _autoCompress = autoCompress;
-        }
-
+        /// <summary>
+        /// Adds an element to the end of the array list.
+        /// </summary>
+        /// <param name="value">The element to add.</param>
         public void Add(T value)
         {
             EnsureSize(_length + 1);
@@ -117,6 +150,12 @@ namespace Alco
             this[_length - 1] = value;
         }
 
+        /// <summary>
+        /// Inserts an element at the specified index.
+        /// </summary>
+        /// <param name="index">The zero-based index at which to insert the element.</param>
+        /// <param name="value">The element to insert.</param>
+        /// <exception cref="IndexOutOfRangeException">Thrown when the index is out of range.</exception>
         public void Insert(int index, T value)
         {
             if (NotInRange(index)) throw new IndexOutOfRangeException(nameof(index));
@@ -126,6 +165,11 @@ namespace Alco
             this[index] = value;
         }
 
+        /// <summary>
+        /// Removes the first occurrence of the specified element from the array list.
+        /// </summary>
+        /// <param name="value">The element to remove.</param>
+        /// <returns>True if the element was successfully removed; otherwise, false.</returns>
         public bool Remove(T value)
         {
             for (int i = 0; i < _length; i++)
@@ -139,14 +183,23 @@ namespace Alco
             return false;
         }
 
+        /// <summary>
+        /// Removes the element at the specified index.
+        /// </summary>
+        /// <param name="index">The zero-based index of the element to remove.</param>
+        /// <exception cref="IndexOutOfRangeException">Thrown when the index is out of range.</exception>
         public void RemoveAt(int index)
         {
             if (NotInRange(index)) throw new IndexOutOfRangeException(nameof(index));
             UtilsMemory.MemCopy((T*)_ptrBuffer + index + 1, (T*)_ptrBuffer + index, sizeof(T) * (_length - index - 1));
-            EnsureSize(_length - 1);
             _length--;
         }
 
+        /// <summary>
+        /// Returns the zero-based index of the first occurrence of the specified element.
+        /// </summary>
+        /// <param name="value">The element to locate.</param>
+        /// <returns>The zero-based index of the first occurrence of the element, or -1 if not found.</returns>
         public int IndexOf(T value)
         {
             for (int i = 0; i < _length; i++)
@@ -159,7 +212,11 @@ namespace Alco
             return -1;
         }
 
-
+        /// <summary>
+        /// Determines whether the array list contains the specified element.
+        /// </summary>
+        /// <param name="value">The element to locate.</param>
+        /// <returns>True if the element is found; otherwise, false.</returns>
         public bool Contains(T value)
         {
             for (int i = 0; i < _length; i++)
@@ -172,12 +229,17 @@ namespace Alco
             return false;
         }
 
+        /// <summary>
+        /// Removes all elements from the array list.
+        /// </summary>
         public void Clear()
         {
-            if (AutoCompress) Resize(DefaultSize);
             _length = 0;
         }
 
+        /// <summary>
+        /// Releases all resources used by the NativeArrayList.
+        /// </summary>
         public void Dispose()
         {
             if (_isDisposed) return;
@@ -207,11 +269,6 @@ namespace Alco
             if (size > _capacity)
             {
                 Resize(_capacity * 2);
-            }
-
-            if (AutoCompress && size > DefaultSize && size < _capacity / 2)
-            {
-                Resize(_capacity / 2);
             }
         }
 
