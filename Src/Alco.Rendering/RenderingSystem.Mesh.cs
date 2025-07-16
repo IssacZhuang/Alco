43835a8e36fc53d6440b701f213a9879f5c3ea48
zhuang jr <1095313400@qq.com>
2025-07-16 11:30:59 +0800

    feature: create mesh API

--- Src/Alco.Rendering/RenderingSystem.Mesh.cs
+++ Src/Alco.Rendering/RenderingSystem.Mesh.cs
@@ -16,8 +16,25 @@ public partial class RenderingSystem
     /// <param name="name">The name of the mesh.</param>
     /// <typeparam name="TVertex">The type of the vertices.</typeparam>
     /// <returns>The created mesh.</returns>
-    public unsafe PrimitiveMesh CreatePrimitiveMesh<TVertex>(TVertex[] vertices, uint[] indices, string name = "mesh") where TVertex : unmanaged
+    public unsafe PrimitiveMesh CreatePrimitiveMesh<TVertex>(ReadOnlySpan<TVertex> vertices, ReadOnlySpan<uint> indices, string name = "mesh") where TVertex : unmanaged
     {
+        PrimitiveMesh mesh = new PrimitiveMesh(_device, (uint)vertices.Length * (uint)sizeof(TVertex), (uint)indices.Length, IndexFormat.UInt32, name);
+        mesh.SetVertex<TVertex>(vertices);
+        mesh.SetIndices(indices);
+        return mesh;
+    }
+
+    /// <summary>
+    /// Create a mesh.
+    /// </summary>
+    /// <param name="vertices">The vertices of the mesh.</param>
+    /// <param name="indices">The indices of the mesh.</param>
+    /// <param name="name">The name of the mesh.</param>
+    /// <typeparam name="TVertex">The type of the vertices.</typeparam>
+    /// <returns></returns>
+    public unsafe PrimitiveMesh CreatePrimitiveMesh<TVertex>(ReadOnlySpan<TVertex> vertices, ReadOnlySpan<ushort> indices, string name = "mesh") where TVertex : unmanaged
+    {
+
         PrimitiveMesh mesh = new PrimitiveMesh(_device, (uint)vertices.Length * (uint)sizeof(TVertex), (uint)indices.Length, IndexFormat.UInt16, name);
         mesh.SetVertex<TVertex>(vertices);
         mesh.SetIndices(indices);
@@ -36,22 +53,7 @@ public partial class RenderingSystem
         return new PrimitiveMesh(_device, vertexBufferSize, indexBufferSize, IndexFormat.UInt16, name);
     }
 
-    /// <summary>
-    /// Create a mesh.
-    /// </summary>
-    /// <param name="vertices">The vertices of the mesh.</param>
-    /// <param name="indices">The indices of the mesh.</param>
-    /// <param name="name">The name of the mesh.</param>
-    /// <typeparam name="TVertex">The type of the vertices.</typeparam>
-    /// <returns></returns>
-    public unsafe PrimitiveMesh CreatePrimitiveMesh<TVertex>(TVertex[] vertices, ushort[] indices, string name = "mesh") where TVertex : unmanaged
-    {
-
-        PrimitiveMesh mesh = new PrimitiveMesh(_device, (uint)vertices.Length * (uint)sizeof(TVertex), (uint)indices.Length, IndexFormat.UInt16, name);
-        mesh.SetVertex<TVertex>(vertices);
-        mesh.SetIndices(indices);
-        return mesh;
-    }
+    
 
     /// <summary>
     /// Create a dynamic mesh with custom vertex and index buffer sizes.
