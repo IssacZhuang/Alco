f1b9ae1cab08ccc81dc938be727cc3621f5f1c93
IssacZhuang <1095313400@qq.com>
2025-07-19 17:38:55 +0800

    add docs for create material

--- Src/Alco.Rendering/RenderingSystem.Material.cs
+++ Src/Alco.Rendering/RenderingSystem.Material.cs
@@ -7,6 +7,17 @@ namespace Alco.Rendering;
 
 public partial class RenderingSystem
 {
+    /// <summary>
+    /// Creates a new graphics material with the specified shader and automatically sets up default properties.
+    /// </summary>
+    /// <param name="shader">The shader to use for the material. Cannot be null.</param>
+    /// <param name="name">The name of the material for debugging purposes. Defaults to "unamed_material".</param>
+    /// <returns>A new <see cref="GraphicsMaterial"/> instance with the specified shader and default buffers configured.</returns>
+    /// <remarks>
+    /// Default properties:
+    /// _camera : camera matrix of <see cref="RenderingSystem.MainCamera"/> 
+    /// _globalRenderData : global render data of <see cref="RenderingSystem.GlobalRenderData"/>
+    /// </remarks>
     public GraphicsMaterial CreateMaterial(Shader shader, string name = "unamed_material")
     {
         Debug.Assert(shader != null);
