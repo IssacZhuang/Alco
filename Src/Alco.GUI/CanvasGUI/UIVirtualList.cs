2716013d868b8375e159c99a1f3d47d6f2eef3d7
zhuang jr <1095313400@qq.com>
2025-09-03 10:42:40 +0800

    feature: virtual list tempate

--- Src/Alco.GUI/CanvasGUI/UIVirtualList.cs
+++ Src/Alco.GUI/CanvasGUI/UIVirtualList.cs
@@ -2,5 +2,43 @@ namespace Alco.GUI;
 
 public abstract class UIVirtualList<TData> : UINode
 {
+    private struct ActiveItem
+    {
+        public UINode Node;
+        public int Index;
+
+        public ActiveItem(UINode node, int index)
+        {
+            Node = node;
+            Index = index;
+        }
+    }
+
+    private readonly Deque<ActiveItem> _activeItems = new();
+
     protected abstract UINode CreateItem();
+
+    protected int GetStartIndex()
+    {
+        //todo
+        return 0;
+    }
+
+    protected int GetEndIndex()
+    {
+        //todo
+        return 0;
+    }
+
+    protected void SetDataForItem(UINode item, int index, TData data)
+    {
+        if (item is IUIListItem<TData> uiListItem)
+        {
+            uiListItem.SetData(index, data);
+        }
+    }
+
+    private class VirtualContainer: UINode{
+        //todo
+    }
 }
