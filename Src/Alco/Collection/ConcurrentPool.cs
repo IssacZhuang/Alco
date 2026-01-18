f7d53b0db169dc3f32818641c326670cc5636235
IssacZhuang <1095313400@qq.com>
2026-01-18 16:35:05 +0800

    fix: dispose of GraphicBufferPool

--- Src/Alco/Collection/ConcurrentPool.cs
+++ Src/Alco/Collection/ConcurrentPool.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Alco;
 
@@ -35,6 +36,13 @@ public class ConcurrentPool<T> : IEnumerable<T>
     /// </summary>
     /// <returns>An object of type <typeparamref name="T"/>.</returns>
     public T Get() => _objects.TryTake(out T? item) ? item : _objectGenerator();
+
+    /// <summary>
+    /// Attempts to get an object from the pool without creating a new one.
+    /// </summary>
+    /// <param name="item">When this method returns, contains the object from the pool if available; otherwise, the default value.</param>
+    /// <returns>true if an object was removed from the pool; otherwise, false.</returns>
+    public bool TryTake([MaybeNullWhen(false)] out T item) => _objects.TryTake(out item);
 
     /// <summary>
     /// Returns an object to the pool.
