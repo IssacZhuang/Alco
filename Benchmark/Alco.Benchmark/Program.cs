b0b55bfed87918d9a3a33b21f3055e860a34c9ae
IssacZhuang <1095313400@qq.com>
2026-05-11 16:36:43 +0800

    feat(benchmark): add --profile flag to route to profiling harness

--- Benchmark/Alco.Benchmark/Program.cs
+++ Benchmark/Alco.Benchmark/Program.cs
@@ -1,11 +1,16 @@
-﻿using BenchmarkDotNet.Analysers;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Exporters;
 using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Reports;
 using BenchmarkDotNet.Running;
 using Alco.Benchmark;
-using BenchmarkDotNet.Reports;
 using BenchmarkFramework;
+
+if (args.Contains("--profile"))
+{
+    ProfileImageDecode.Run(args);
+    return;
+}
 
 IConfig config = new DefaultBenchmarkConfig();
 
